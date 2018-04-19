d7003bc9e8d6c5d19719b77de1ff1d8864fa2e6c
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-04-19 11:31:16 +0300

    Applied 'square' style to all cards

--- ui/src/cljs/lipas_ui/open_data/views.cljs
+++ ui/src/cljs/lipas_ui/open_data/views.cljs
@@ -8,36 +8,37 @@
             :lipas "http://lipas.cc.jyu.fi/api/index.html"})
 
 (defn create-panel []
-  [mui/grid {:container true}
-   [mui/grid {:item true :xs 12}
-    [mui/card
-     [mui/card-content
-      [mui/typography {:variant "headline"} "Rajapinnat"]
-      [mui/typography "Kaikki data on avointa blabalba."]]]]
-   [mui/grid {:item true :xs 12 :md 6 :lg 4}
-    [mui/card
-     [mui/card-content
-      [mui/typography {:variant "headline"} "REST"]
-      [mui/list
-       [mui/list-item {:button true
-                       :on-click #(navigate! {:lipas links})}
-        [mui/list-item-icon
-         [mui-icons/build]]
-        [mui/list-item-text {:primary "Swagger"}]]
-       [mui/list-item {:button true
-                       :on-click #(navigate! (:github links))}
-        [mui/list-item-icon
-         [mui/svg-icon
-          svg/github-icon]]
-        [mui/list-item-text {:primary "GitHub"}]]]]]]
-   [mui/grid {:item true :xs 12 :md 6 :lg 4}
-    [mui/card
-     [mui/card-content
-      [mui/typography {:variant "headline"} "WMS"]]]]
-   [mui/grid {:item true :xs 12 :md 6 :lg 4}
-    [mui/card
-     [mui/card-content
-      [mui/typography {:variant "headline"} "WFS"]]]]])
+  (let [card-props {:square true}]
+    [mui/grid {:container true}
+     [mui/grid {:item true :xs 12}
+      [mui/card card-props
+       [mui/card-content
+        [mui/typography {:variant "headline"} "Rajapinnat"]
+        [mui/typography "Kaikki data on avointa blabalba."]]]]
+     [mui/grid {:item true :xs 12 :md 6 :lg 4}
+      [mui/card card-props
+       [mui/card-content
+        [mui/typography {:variant "headline"} "REST"]
+        [mui/list
+         [mui/list-item {:button true
+                         :on-click #(navigate! {:lipas links})}
+          [mui/list-item-icon
+           [mui-icons/build]]
+          [mui/list-item-text {:primary "Swagger"}]]
+         [mui/list-item {:button true
+                         :on-click #(navigate! (:github links))}
+          [mui/list-item-icon
+           [mui/svg-icon
+            svg/github-icon]]
+          [mui/list-item-text {:primary "GitHub"}]]]]]]
+     [mui/grid {:item true :xs 12 :md 6 :lg 4}
+      [mui/card card-props
+       [mui/card-content
+        [mui/typography {:variant "headline"} "WMS"]]]]
+     [mui/grid {:item true :xs 12 :md 6 :lg 4}
+      [mui/card card-props
+       [mui/card-content
+        [mui/typography {:variant "headline"} "WFS"]]]]]))
 
 (defn main []
   (create-panel))
