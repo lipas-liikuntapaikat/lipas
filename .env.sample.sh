33ff545b9bcd2948f381397244875b2774f564bc
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-04-21 15:34:04 +0300

    Enhanced db setup script

--- .env.sample.sh
+++ .env.sample.sh
@@ -4,5 +4,6 @@ export DB_NAME=lipas
 export DB_HOST=localhost
 export DB_PORT=5432
 export DB_SUPERUSER=postgres
+export DB_SUPERUSER_PASSWORD=FILL_THIS
 export DB_USER=lipas
 export DB_PASSWORD=FILL_THIS
