1a89e4e3942fd373eb3928c1d30177aabf063073
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-05-03 14:24:18 +0300

    Login automatically after successful registration

--- ui/src/cljs/lipas_ui/register/events.cljs
+++ ui/src/cljs/lipas_ui/register/events.cljs
@@ -1,5 +1,7 @@
 (ns lipas-ui.register.events
   (:require [re-frame.core :as re-frame]
+            [lipas-ui.db :refer [default-db]]
+            [lipas-ui.login.events :as login-events]
             [ajax.core :as ajax]))
 
 (re-frame/reg-event-db
@@ -8,10 +10,17 @@
    (let [path (into [:user :registration-form] path)]
      (assoc-in db path value))))
 
-(re-frame/reg-event-db
+(re-frame/reg-event-fx
  ::registration-success
- (fn [db [_ result]]
-   (assoc-in db [:user :registration] result)))
+ (fn [{:keys [db]} [_ result]]
+   (let [username   (-> db :user :registration-form :username)
+         password   (-> db :user :registration-form :password)
+         empty-form (-> default-db :user :registration-form)]
+     {:db (-> db
+           (assoc-in [:user :registration] result)
+           (assoc-in [:user :registration-form] empty-form))
+      :dispatch [::login-events/submit-login-form {:username username
+                                                   :password password}]})))
 
 (re-frame/reg-event-db
  ::registration-failure
