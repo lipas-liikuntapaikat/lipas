61a74ae8a5f7b248b6506a0c792d768b318faadd
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-05-23 18:48:07 +0300

    Cleaned up sample env vars

--- .env.sample.sh
+++ .env.sample.sh
@@ -4,22 +4,12 @@
 
 # Postgres
 export DB_NAME=lipas
-export DB_HOST=localhost
+export DB_HOST=postgres
 export DB_PORT=5432
 export DB_SUPERUSER=postgres
 export DB_SUPERUSER_PASSWORD=***FILL_THIS***
 export DB_USER=lipas
 export DB_PASSWORD=***FILL_THIS***
 
-# auth-service
-export AUTH_DB_NAME=auth_service
-export AUTH_DB_USER=auth_service_user
-export AUTH_DB_PASSWORD=***FILL_THIS***
-export AUTH_DB_URL=postgres://$AUTH_DB_USER:$AUTH_DB_PASSWORD@postgres:5432/$AUTH_DB_NAME?stringtype=unspecified
-export AUTH_EMAIL_USER=***FILL_THIS***
-export AUTH_EMAIL_PASSWORD=***FILL_THIS***
-export AUTH_TOKEN_PRIVATE_KEY=***FILL_THIS***
-
-# auth-service tests
-export AUTH_TEST_DB_NAME=auth_service_test
-export AUTH_TEST_DB_URL=postgres://$AUTH_DB_USER:$AUTH_DB_PASSWORD@postgres:5432/$AUTH_TEST_DB_NAME?stringtype=unspecified
+# Migratus
+export DB_URL=postgres://$DB_USER:$DB_PASSWORD@postgres:$DB_PORT/$DB_NAME?stringtype=unspecified
