2e38a446292b2c43e9164e89e685cd654f3d176b
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-05-25 19:23:17 +0300

    Added initial version of ice stadiums basic info form

--- webapp/src/cljs/lipas/ui/ice_stadiums/events.cljs
+++ webapp/src/cljs/lipas/ui/ice_stadiums/events.cljs
@@ -1,7 +1,32 @@
 (ns lipas.ui.ice-stadiums.events
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [lipas.ui.utils :refer [save-entity]]))
 
 (re-frame/reg-event-db
  ::set-active-tab
  (fn [db [_ active-tab]]
    (assoc-in db [:ice-stadiums :active-tab] active-tab)))
+
+(re-frame/reg-event-db
+ ::set-field
+ (fn [db [_ path value]]
+   (assoc-in db (into [:ice-stadiums] path) value)))
+
+(re-frame/reg-event-db
+ ::toggle-dialog
+ (fn [db [_ dialog data]]
+   (let [data (or data (-> db :ice-stadiums :dialogs dialog :data))]
+     (-> db
+         (update-in [:ice-stadiums :dialogs dialog :open?] not)
+         (assoc-in [:ice-stadiums :dialogs dialog :data] data)))))
+
+(re-frame/reg-event-db
+ ::save-renovation
+ (fn [db [_ value]]
+   (let [path [:ice-stadiums :editing :renovations]]
+     (save-entity db path value))))
+
+(re-frame/reg-event-db
+ ::remove-renovation
+ (fn [db [_ {:keys [id]}]]
+   (update-in db [:ice-stadiums :editing :renovations] dissoc id)))
