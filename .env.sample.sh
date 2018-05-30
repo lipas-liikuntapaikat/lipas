a1f4d487b9ada1507b79513baa9402c2cf98999b
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-05-30 15:44:48 +0300

    Added mapproxy env vars to .env.sample.sh

--- .env.sample.sh
+++ .env.sample.sh
@@ -10,3 +10,8 @@ export DB_SUPERUSER=postgres
 export DB_SUPERUSER_PASSWORD=***FILL_THIS***
 export DB_USER=lipas
 export DB_PASSWORD=***FILL_THIS***
+
+# Mapproxy
+MML_USERNAME=***FILL_THIS***
+MML_PASSWORD=***FILL_THIS***
+export MML_AUTH=`echo -n $MML_USERNAME:$MML_PASSWORD | base64`
