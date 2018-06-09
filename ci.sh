d4d9883b7f380699d46301c9111223241a9305c6
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-06-09 21:56:59 +0300

    Added nice headings to Travis output

--- ci.sh
+++ ci.sh
@@ -2,9 +2,15 @@
 
 set -e
 
-# Backend
+### Backend ###
+
+echo '\n *** Running backend migrations *** \n'
 docker-compose run backend-migrate
+
+echo '\n *** Running backend tests *** \n'
 docker-compose run backend-test
 
-# Frontend
+### Frontend ###
+
+echo '\n *** Compiling frontend *** \n'
 docker-compose run frontend-build
