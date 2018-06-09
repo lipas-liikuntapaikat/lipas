fc985eb4a991eecda7e9398d4c8837f475e82396
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-06-09 21:59:38 +0300

    Changed ' to " to not print '\n' characters.

--- ci.sh
+++ ci.sh
@@ -4,13 +4,13 @@ set -e
 
 ### Backend ###
 
-echo '\n *** Running backend migrations *** \n'
+echo "\n *** Running backend migrations *** \n"
 docker-compose run backend-migrate
 
-echo '\n *** Running backend tests *** \n'
+echo "\n *** Running backend tests *** \n"
 docker-compose run backend-test
 
 ### Frontend ###
 
-echo '\n *** Compiling frontend *** \n'
+echo "\n *** Compiling frontend *** \n"
 docker-compose run frontend-build
