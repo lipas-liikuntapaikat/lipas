51873d6818aa23fc21e74cbed780d4c560fad19f
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-06-09 22:24:17 +0300

    Revert "Added nice color to travis output headings"
    This reverts commit e864c9dd854b664b7bfedd74c53d47ee30a56532.
    
    Colors didn't show up in Travis. :/

--- ci.sh
+++ ci.sh
@@ -2,18 +2,15 @@
 
 set -e
 
-NORMAL=$(tput sgr0)
-LIME_YELLOW=$(tput setaf 190)
-
 ### Backend ###
 
-printf "\n ${LIME_YELLOW}*** Running backend migrations ***${NORMAL} \n\n"
+printf "\n *** Running backend migrations *** \n\n"
 docker-compose run backend-migrate
 
-printf "\n ${LIME_YELLOW}*** Running backend tests ***${NORMAL} \n\n"
+printf "\n *** Running backend tests *** \n\n"
 docker-compose run backend-test
 
 ### Frontend ###
 
-printf "\n ${LIME_YELLOW}*** Compiling frontend ***${NORMAL} \n\n"
+printf "\n *** Compiling frontend *** \n\n"
 docker-compose run frontend-build
