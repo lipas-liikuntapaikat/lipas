2ef7c8e5e5fe67ac4a0f64c74e988d833fa7614a
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-06-12 12:21:32 +0300

    Added links to sub-pages to front-page

--- webapp/src/cljs/lipas/ui/front_page/views.cljs
+++ webapp/src/cljs/lipas/ui/front_page/views.cljs
@@ -1,13 +1,21 @@
 (ns lipas.ui.front-page.views
-  (:require [lipas.ui.mui :as mui]))
+  (:require [lipas.ui.mui :as mui]
+            [lipas.ui.mui-icons :as mui-icons]
+            [lipas.ui.routes :refer [navigate!]]
+            [reagent.core :as r]))
 
-(defn grid-card [{:keys [title style]} & children]
+(defn grid-card [{:keys [title style link]} & children]
   [mui/grid {:item true :xs 12 :md 6 :lg 4}
    [mui/card {:square true
               :style (merge style {:height "100%"})}
-    [mui/card-header {:title title}]
-    (into [mui/card-content]
-          children)]])
+    [mui/card-header {:title title
+                      :action (when link
+                                (r/as-element [mui/icon-button
+                                               {:on-click #(navigate! link)
+                                                :color "secondary"}
+                                               [mui-icons/arrow-forward]]))}]
+    (into [mui/card-content] children)
+    [mui/card-actions]]])
 
 (defn create-panel [tr]
   [:div {:style {:padding "1em"}}
@@ -16,18 +24,20 @@
     [grid-card {:style {:background-color "#f7ed33"}
                 :title (tr :disclaimer/headline)}
      [mui/typography (tr :disclaimer/test-version)]]
-    [grid-card {:title (tr :sport/headline)}
-     [mui/typography (tr :sport/description)]
-     ;; http://lipas.cc.jyu.fi/lipas/VAADIN/themes/lipas/img/lipaskuva.jpg
-     ;; [mui/card-media {:image "img/lipas_kuva_2018.jpg"}]
-     ]
-    [grid-card {:title (tr :ice/headline)}
+    [grid-card {:title (tr :sport/headline)
+                :link "/#/liikuntapaikat"}
+     [mui/typography (tr :sport/description)]]
+    [grid-card {:title (tr :ice/headline)
+                :link "/#/jaahalliportaali"}
      [mui/typography (tr :ice/description)]]
-    [grid-card {:title (tr :swim/headline)}
+    [grid-card {:title (tr :swim/headline)
+                :link "/#/uimahalliportaali"}
      [mui/typography (tr :swim/description)]]
-    [grid-card {:title (tr :open-data/headline)}
+    [grid-card {:title (tr :open-data/headline)
+                :link "/#/avoin-data"}
      [mui/typography (tr :open-data/description)]]
-    [grid-card {:title (tr :help/headline)}
+    [grid-card {:title (tr :help/headline)
+                :link "/#/ohjeet"}
      [mui/typography (tr :help/description)]]]])
 
 (defn main [tr]
