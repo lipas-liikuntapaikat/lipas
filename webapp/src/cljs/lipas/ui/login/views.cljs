50b2c56864e5fdf97f29cdda07f7d5355e468fe4
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-06-12 14:32:29 +0300

    Improved login page usability

--- webapp/src/cljs/lipas/ui/login/views.cljs
+++ webapp/src/cljs/lipas/ui/login/views.cljs
@@ -1,10 +1,11 @@
 (ns lipas.ui.login.views
-  (:require [lipas.ui.mui :as mui]
-            [lipas.ui.components :as lui]
-            [lipas.ui.utils :refer [<== ==>]]
+  (:require [lipas.ui.components :as lui]
             [lipas.ui.login.events :as events]
             [lipas.ui.login.subs :as subs]
-            [lipas.ui.routes :refer [navigate!]]))
+            [lipas.ui.mui :as mui]
+            [reagent.core :as r]
+            [lipas.ui.routes :refer [navigate!]]
+            [lipas.ui.utils :refer [<== ==>]]))
 
 (defn set-field [& args]
   (==> [::events/set-field (butlast args) (last args)]))
@@ -12,34 +13,54 @@
 (defn clear-errors []
   (==> [::events/clear-errors]))
 
-(defn create-login-form [tr {:keys [username password] :as form-data} error]
-  [mui/form-group
-   [lui/text-field {:label (tr :login/username)
-                    :auto-focus true
-                    :type "text"
-                    :value username
-                    :on-change (comp clear-errors #(set-field :username %))
-                    :required true
-                    :placeholder (tr :login/username-example)}]
-   [lui/text-field {:label (tr :login/password)
-                    :type "password"
-                    :value password
-                    :on-change (comp clear-errors #(set-field :password %))
-                    :required true}]
-   [mui/button {:color "primary"
-                :disabled (or (empty? username)
-                              (empty? password)
-                              (some? error))
-                :size "large"
-                :on-click #(==> [::events/submit-login-form form-data])}
-    (tr :login/login)]
-   (when error
-     [mui/typography {:color "error"}
-      (case (-> error :response :error)
-        "Not authorized" (tr :login/bad-credentials)
-        (tr :error/unknown))])])
+(defn login-form [{:keys [tr]}]
+  (let [form-data  (<== [::subs/login-form])
+        error      (<== [::subs/login-error])]
 
-(defn create-login-panel [tr form-data error]
+    [mui/form-group
+
+     ;; Username
+     [lui/text-field
+      {:label (tr :login/username)
+       :auto-focus true
+       :value (:username form-data)
+       :on-change (comp clear-errors #(set-field :username %))
+       :required true
+       :placeholder (tr :login/username-example)}]
+
+     ;; Password
+     [lui/text-field
+      {:label (tr :login/password)
+       :type "password"
+       :value (:password form-data)
+       :on-change (comp clear-errors #(set-field :password %))
+       :required true}]
+
+     ;; Login button
+     [mui/button
+      {:color "secondary"
+       :style {:margin-top "1em"}
+       :full-width true
+       :size "large"
+       :variant "raised"
+       :on-click #(==> [::events/submit-login-form form-data])}
+      (tr :login/login)]
+
+     ;; Error messages
+     (when error
+       [mui/typography {:color "error"}
+        (case (-> error :response :error)
+          "Not authorized" (tr :login/bad-credentials)
+          (tr :error/unknown))])]))
+
+(defn register-btn [{:keys [tooltip]}]
+  [mui/tooltip
+   {:title tooltip}
+   [mui/icon-button
+    { :on-click #(navigate! "/#/rekisteroidy")}
+    [mui/icon "group_add"]]])
+
+(defn login-panel [{:keys [tr]}]
   (let [card-props {:square true
                     :style {:height "100%"}}]
     [mui/grid {:container true
@@ -47,14 +68,15 @@
                :style {:padding "1em"}}
      [mui/grid {:item true :xs 12 :md 8 :lg 6}
       [mui/card card-props
-       [mui/card-header {:title (tr :login/headline)}]
+       [mui/card-header {:title (tr :login/headline)
+                         :action (r/as-element
+                                  [register-btn
+                                   {:tooltip (tr :register/headline)}])}]
        [mui/card-content
-        (create-login-form tr form-data error)]]]]))
+        [login-form {:tr tr}]]]]]))
 
 (defn main [tr]
-  (let [logged-in? (<== [::subs/logged-in?])
-        form-data  (<== [::subs/login-form])
-        error      (<== [::subs/login-error])]
+  (let [logged-in? (<== [::subs/logged-in?])]
     (if logged-in?
       (navigate! "/#/profiili")
-      (create-login-panel tr form-data error))))
+      [login-panel {:tr tr}])))
