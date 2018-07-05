f857cc50ebac8227084beb99d08c340116ef71e0
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-07-05 15:34:35 +0300

    Disabled "my info" form for now since it's not functional.

--- webapp/src/cljs/lipas/ui/user/views.cljs
+++ webapp/src/cljs/lipas/ui/user/views.cljs
@@ -1,41 +1,47 @@
 (ns lipas.ui.user.views
   (:require [lipas.ui.mui :as mui]
             [lipas.ui.components :as lui]
-            [cljs.pprint :as pp]
-            ;; [lipas.ui.user.events :as events]
             [lipas.ui.user.subs :as subs]
             [lipas.ui.routes :refer [navigate!]]
             [re-frame.core :as re-frame]))
 
-(defn create-user-panel [tr data]
+(defn user-panel [tr data]
   (let [card-props {:square true
-                    :style {:height "100%"}}
-        firstname (-> data :user-data :firstname)
-        lastname (-> data :user-data :lastname)]
+                    :style  {:height "100%"}}
+        firstname  (-> data :user-data :firstname)
+        lastname   (-> data :user-data :lastname)]
     [mui/grid {:container true
-               :justify "center"
-               :style {:padding "1em"}}
+               :justify   "center"
+               :style     {:padding "1em"}}
      [mui/grid {:item true :xs 12 :md 6}
       [mui/card card-props
-       [mui/card-header {:title (str "Hei " firstname " " lastname "!")}]
+       [mui/card-header {:title (tr :user/greeting firstname lastname)}]
        [mui/card-content
         [mui/form-group
-         [lui/text-field {:label (tr :register/username)
-                          :value (:username data)}]
-         [lui/text-field {:label (tr :register/firstname)
-                          :value firstname}]
-         [lui/text-field {:label (tr :register/lastname)
-                          :value lastname}]
-         [lui/text-field {:label (tr :register/permissions)
-                          :value (pr-str (:permissions data))}]
-         [lui/text-field {:label (tr :user/requested-permissions)
-                          :value (-> data :user-data :permissions-request)}]]
-        ;;[:pre (with-out-str (pp/pprint data))]
-        ]]]]))
+         [lui/text-field
+          {:label    (tr :register/username)
+           :value    (:username data)
+           :disabled true}]
+         [lui/text-field
+          {:label    (tr :register/firstname)
+           :value    firstname
+           :disabled true}]
+         [lui/text-field
+          {:label    (tr :register/lastname)
+           :value    lastname
+           :disabled true}]
+         [lui/text-field
+          {:label    (tr :register/permissions)
+           :value    (pr-str (:permissions data))
+           :disabled true}]
+         [lui/text-field
+          {:label    (tr :user/requested-permissions)
+           :value    (-> data :user-data :permissions-request)
+           :disabled true}]]]]]]))
 
 (defn main [tr]
   (let [logged-in? (re-frame/subscribe [::subs/logged-in?])
-        user-data (re-frame/subscribe [::subs/user-data])]
+        user-data  (re-frame/subscribe [::subs/user-data])]
     (if @logged-in?
-      (create-user-panel tr @user-data)
+      [user-panel tr @user-data]
       (navigate! "/#/kirjaudu"))))
