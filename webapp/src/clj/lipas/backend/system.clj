525931bcf6433706aba0717136a6c37e3acbcfcf
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-07-08 19:43:35 +0300

    Mask secrets when printing config

--- webapp/src/clj/lipas/backend/system.clj
+++ webapp/src/clj/lipas/backend/system.clj
@@ -6,12 +6,12 @@
             [lipas.backend.handler :as handler]))
 
 (def config
-  {:db {:dbtype "postgresql"
-        :dbname (:db-name env)
-        :host (:db-host env)
-        :user (:db-user env)
-        :port (:db-port env)
-        :password (:db-password env)}
+  {:db  {:dbtype   "postgresql"
+         :dbname   (:db-name env)
+         :host     (:db-host env)
+         :user     (:db-user env)
+         :port     (:db-port env)
+         :password (:db-password env)}
    :app {:db (ig/ref :db)}})
 
 (defmethod ig/init-key :db [_ db-spec]
@@ -20,8 +20,12 @@
 (defmethod ig/init-key :app [_ config]
   (handler/create-app config))
 
+(defn mask [s]
+  "[secret]")
+
 (defn start-system! []
   (let [system (ig/init config)]
     (prn "System started with config:")
-    (pprint config)
+    (pprint (-> config
+                (update-in [:db :password] mask)))
     system))
