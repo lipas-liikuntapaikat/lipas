f56b1cfb6d267f43324828bfb6abf4b56efb2e78
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-07-16 14:48:52 +0300

    Switched "edn in text column" to native jsonb in db

--- webapp/src/clj/lipas/backend/db/user.clj
+++ webapp/src/clj/lipas/backend/db/user.clj
@@ -1,29 +1,17 @@
 (ns lipas.backend.db.user
-  (:require [camel-snake-kebab.core :refer [->kebab-case ->snake_case]]
-            [camel-snake-kebab.extras :refer [transform-keys]]
-            [clojure.edn :as edn]
-            [hugsql.core :as hugsql]))
-
-(defn ->kebab-case-keywords [user]
-  (transform-keys ->kebab-case user))
-
-(defn ->snake-case-keywords [user]
-  (transform-keys ->snake_case user))
+  (:require [hugsql.core :as hugsql]
+            [lipas.backend.db.utils :as utils]))
 
 (comment (marshall {:kissa_koira "Kana"}))
 (defn marshall [user]
   (-> user
-      (update :permissions pr-str)
-      (update :user-data pr-str)
-      (->snake-case-keywords)))
+      (utils/->snake-case-keywords)))
 
 (comment (unmarshall {:kissa_koira "Kana"}))
 (comment (unmarshall nil))
 (comment (unmarshall {}))
 (defn unmarshall [user]
   (some-> (not-empty user)
-          (->kebab-case-keywords)
-          (update :permissions edn/read-string)
-          (update :user-data edn/read-string)))
+          (utils/->kebab-case-keywords)))
 
 (hugsql/def-db-fns "sql/user.sql")
---
+++ webapp/src/clj/lipas/backend/db/utils.clj
@@ -0,0 +1,35 @@
+(ns lipas.backend.db.utils
+  (:require [camel-snake-kebab.core :refer [->kebab-case ->snake_case]]
+            [camel-snake-kebab.extras :refer [transform-keys]]
+            [cheshire.core :as j]
+            [clojure.java.jdbc :as jdbc])
+  (:import [org.postgresql.util PGobject]))
+
+(comment (<-json (->json {:kissa "koira"})))
+(def <-json #(j/decode % true))
+(def ->json j/encode)
+
+(defn ->kebab-case-keywords [user]
+  (transform-keys ->kebab-case user))
+
+(defn ->snake-case-keywords [user]
+  (transform-keys ->snake_case user))
+
+(defn ->pgobject [m]
+  (doto (PGobject.)
+    ;; hack for now -- eventually we should properly determine the actual type
+    (.setType "jsonb")
+    (.setValue (->json m))))
+
+(extend-protocol jdbc/ISQLValue
+  clojure.lang.IPersistentMap
+  (sql-value [m] (->pgobject m)))
+
+(extend-protocol jdbc/IResultSetReadColumn
+  org.postgresql.util.PGobject
+  (result-set-read-column [pgobj metadata idx]
+    (let [type  (.getType pgobj)
+          value (.getValue pgobj)]
+      (if (#{"jsonb" "json"} type)
+        (<-json value)
+        value))))
