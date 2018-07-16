c10d120c8e3da001811bc71fa525ce61326e07c6
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-07-16 14:50:11 +0300

    Added table for sports-sites

--- webapp/resources/migrations/20180521143603-initial.up.sql
+++ webapp/resources/migrations/20180521143603-initial.up.sql
@@ -18,3 +18,29 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.account
 OWNER to lipas;
+
+-- Table: public.sports_site
+
+-- DROP TABLE public.sports_site;
+
+CREATE TABLE public.sports_site (
+created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+lipas_id   integer NOT NULL,
+status     text COLLATE pg_catalog."default" NOT NULL,
+document   jsonb NOT NULL,
+author_id  uuid NOT NULL,
+type_code  integer NOT NULL,
+city_code  text COLLATE pg_catalog."default" NOT NULL,
+
+CONSTRAINT "sports-site_pkey" PRIMARY KEY (created_at, lipas_id),
+CONSTRAINT author_id_account_fk FOREIGN KEY (author_id)
+REFERENCES public.account (id) MATCH SIMPLE
+ON UPDATE NO ACTION
+ON DELETE NO ACTION
+) WITH (
+OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.sports_site
+OWNER to lipas;
