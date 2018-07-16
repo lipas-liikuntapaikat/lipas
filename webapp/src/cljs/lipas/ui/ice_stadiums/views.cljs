ee9d50ff6c264d88beb128e579f8de21f1df4e5f
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-07-16 17:28:40 +0300

    Fixed ice-stadiums table headers

--- webapp/src/cljs/lipas/ui/ice_stadiums/views.cljs
+++ webapp/src/cljs/lipas/ui/ice_stadiums/views.cljs
@@ -506,12 +506,12 @@
      [mui/grid {:item true :xs 12}
       [mui/paper
        [lui/table
-        {:headers   [[:name (tr :sports-site/name)]
-                     [:address (tr :location/address)]
-                     [:postal-code (tr :location/postal-code)]
-                     [:city (tr :location/city)]
-                     [:admin (tr :sports-site/admin)]
-                     [:owner (tr :sports-site/owner)]]
+        {:headers   [[:name (tr :lipas.sports-site/name)]
+                     [:address (tr :lipas.location/address)]
+                     [:postal-code (tr :lipas.location/postal-code)]
+                     [:city (tr :lipas.location/city)]
+                     [:admin (tr :lipas.sports-site/admin)]
+                     [:owner (tr :lipas.sports-site/owner)]]
          :items     sites
          :on-select #(==> [::events/display-site %])}]]]]))
 
