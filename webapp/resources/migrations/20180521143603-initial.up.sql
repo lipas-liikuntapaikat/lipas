449a5a9bd2c1d4f3361ccc4f2848e1a0152b0953
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-07-16 14:48:52 +0300

    Switched "edn in text column" to native jsonb in db

--- webapp/resources/migrations/20180521143603-initial.up.sql
+++ webapp/resources/migrations/20180521143603-initial.up.sql
@@ -1,10 +1,20 @@
-CREATE TABLE IF NOT EXISTS account (
-id              UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4()
-, created_on    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-, email         TEXT               NOT NULL UNIQUE
-, username      TEXT               NOT NULL UNIQUE
-, password      TEXT               NOT NULL
-, permissions   TEXT
-, user_data     TEXT
-, refresh_token TEXT
-);
+CREATE TABLE public.account (
+  id            uuid NOT NULL DEFAULT uuid_generate_v4(),
+  created_at    timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+  email         text COLLATE pg_catalog."default" NOT NULL,
+  username      text COLLATE pg_catalog."default" NOT NULL,
+  password      text COLLATE pg_catalog."default" NOT NULL,
+  permissions   jsonb,
+  user_data     jsonb,
+  refresh_token text COLLATE pg_catalog."default",
+
+  CONSTRAINT account_pkey PRIMARY KEY (id),
+  CONSTRAINT account_email_key UNIQUE (email),
+  CONSTRAINT account_username_key UNIQUE (username)
+) WITH (
+  OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.account
+OWNER to lipas;
