602abfec13eaf290b3991c9ce1347bc95610fe81
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-07-21 12:00:03 +0300

    Fixed circular dependency error

--- webapp/src/cljs/lipas/ui/routes.cljs
+++ webapp/src/cljs/lipas/ui/routes.cljs
@@ -1,11 +1,10 @@
 (ns lipas.ui.routes
   (:require-macros [secretary.core :refer [defroute]])
   (:import goog.History)
-  (:require [secretary.core :as secretary]
-            [goog.events :as gevents]
+  (:require [goog.events :as gevents]
             [goog.history.EventType :as EventType]
-            [re-frame.core :as re-frame]
-            [lipas.ui.events :as events]))
+            [lipas.ui.utils :refer [==>]]
+            [secretary.core :as secretary]))
 
 (defn navigate! [path]
   (set! (.-location js/window) path))
@@ -23,31 +22,31 @@
   ;; --------------------
   ;; define routes here
   (defroute "/" []
-    (re-frame/dispatch [::events/set-active-panel :home-panel]))
+    (==> [:lipas.ui.events/set-active-panel :home-panel]))
 
   (defroute "/liikuntapaikat" []
-    (re-frame/dispatch [::events/set-active-panel :sports-panel]))
+    (==> [:lipas.ui.events/set-active-panel :sports-panel]))
 
   (defroute "/jaahalliportaali" []
-    (re-frame/dispatch [::events/set-active-panel :ice-panel]))
+    (==> [:lipas.ui.events/set-active-panel :ice-panel]))
 
   (defroute "/uimahalliportaali" []
-    (re-frame/dispatch [::events/set-active-panel :swim-panel]))
+    (==> [:lipas.ui.events/set-active-panel :swim-panel]))
 
   (defroute "/avoin-data" []
-    (re-frame/dispatch [::events/set-active-panel :open-data-panel]))
+    (==> [:lipas.ui.events/set-active-panel :open-data-panel]))
 
   (defroute "/ohjeet" []
-    (re-frame/dispatch [::events/set-active-panel :help-panel]))
+    (==> [:lipas.ui.events/set-active-panel :help-panel]))
 
   (defroute "/kirjaudu" []
-    (re-frame/dispatch [::events/set-active-panel :login-panel]))
+    (==> [:lipas.ui.events/set-active-panel :login-panel]))
 
   (defroute "/rekisteroidy" []
-    (re-frame/dispatch [::events/set-active-panel :register-panel]))
+    (==> [:lipas.ui.events/set-active-panel :register-panel]))
 
   (defroute "/profiili" []
-    (re-frame/dispatch [::events/set-active-panel :user-panel]))
+    (==> [:lipas.ui.events/set-active-panel :user-panel]))
 
   ;; --------------------
   (hook-browser-navigation!))
