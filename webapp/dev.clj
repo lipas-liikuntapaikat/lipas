a0ff688b38edc6eea35f49c6cedc9bc2f648de65
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-08-03 18:50:42 +0300

    Deleted duplicate dev.clj

--- webapp/dev.clj
+++
@@ -1,7 +0,0 @@
-(ns lipas.dev
-  (:require [lipas.backend.system :as backend]
-            [ring.middleware.reload :refer [wrap-reload]]))
-
-(def system (backend/start-system!))
-(def app (:app system))
-(def dev-handler (-> #'app wrap-reload))
