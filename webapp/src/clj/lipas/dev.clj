dfb22ae1a42eb19203de99d538d48d61d7ab0047
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-08-03 13:01:12 +0300

    Fixed backend reload setup

--- webapp/src/clj/lipas/dev.clj
+++ webapp/src/clj/lipas/dev.clj
@@ -2,6 +2,6 @@
   (:require [lipas.backend.system :as backend]
             [ring.middleware.reload :refer [wrap-reload]]))
 
-(def system (backend/start-system!))
+(def system (backend/start-system! (dissoc backend/default-config :server)))
 (def app (:app system))
 (def dev-handler (-> #'app wrap-reload))
