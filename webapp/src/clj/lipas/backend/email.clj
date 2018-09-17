433c3ec8e1c24f143ba8b405395bdc18500b38a6
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-09-17 12:52:35 +0300

    Added templates for magic link emails

--- webapp/src/clj/lipas/backend/email.clj
+++ webapp/src/clj/lipas/backend/email.clj
@@ -1,9 +1,16 @@
 (ns lipas.backend.email
   (:require [clojure.string :as str]
+            [clojure.java.io :as io]
             [postal.core :as postal]))
 
 (defprotocol Emailer
   (send! [config message]))
+
+(def templates
+  {:fi
+   {:magic-link
+    {:html (slurp (io/resource "email_templates/magic_link_fi.html"))
+     :text (slurp (io/resource "email_templates/magic_link_fi.txt"))}}})
 
 (defn send*!
   "Thin wrapper for postal."
@@ -28,10 +35,18 @@
                    :html    (str "<html><body>" reset-link "</body></html>")}))
 
 (defn send-magic-login-email! [emailer to link]
-  (.send! emailer {:subject "Taikalinkki Lipakseen"
+  (.send! emailer {:subject "Jää- ja uimahalliportaalit ovat nyt Lipaksessa"
                    :to      to
-                   :plain   (str link)
-                   :html    (str "<html><body>" link "</body></html>")}))
+                   :plain   (-> templates
+                                :fi
+                                :magic-link
+                                :text
+                                (str/replace "{{link}}" link))
+                   :html    (-> templates
+                                :fi
+                                :magic-link
+                                :html
+                                (str/replace "{{link}}" link))}))
 
 (defrecord SMTPEmailer [config]
   Emailer
