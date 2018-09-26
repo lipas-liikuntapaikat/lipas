0ed617d2a1779a9bd631388dc8dabaf309664a2a
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-09-26 10:55:52 +0300

    Added base layer switcher

--- webapp/src/cljs/lipas/ui/map/views.cljs
+++ webapp/src/cljs/lipas/ui/map/views.cljs
@@ -4,17 +4,41 @@
             [lipas.ui.map.map :as ol-map]
             [lipas.ui.map.subs :as subs]
             [lipas.ui.mui :as mui]
+            [reagent.core :as r]
+            [clojure.set :refer [map-invert]]
             [lipas.ui.utils :refer [<== ==>] :as utils]))
 
-(defn tools []
+(defn layer-switcher []
+  (let [basemaps {:taustakartta "Taustakartta"
+                  :maastokartta "Maastokartta"
+                  :ortokuva     "Ortokuva"}
+        basemap  (<== [::subs/basemap])]
+    [mui/form-control {:component "fieldset"}
+     (into
+      [mui/radio-group
+       {:value     (get basemaps basemap)
+        :on-change #(==> [::events/select-basemap (get (map-invert basemaps) %2)])}]
+      (for [k (vals basemaps)]
+        [mui/form-control-label {:control (r/as-element [mui/radio])
+                                 :value   k
+                                 :label   k}]))]))
+
+(defn floating-container [{:keys [top right bottom left]} & children]
+  (into
+   [mui/paper {:style {:position         :fixed
+                       :z-index          9999
+                       :background-color mui/gray2
+                       :top              top
+                       :right            right
+                       :bottom           bottom
+                       :left             left
+                       :padding-left     "1em"}}]
+   children))
+
+(defn filters []
   (let [filters (<== [::subs/filters])
         toggle  #(==> [::events/toggle-filter %])]
-    [mui/paper {:style {:position         :fixed
-                        :z-index          9999
-                        :background-color mui/gray2
-                        :bottom           0
-                        :left             0
-                        :padding-left     "1em"}}
+    [:div
      [lui/checkbox
       {:value     (-> filters :ice-stadium)
        :label     "Jäähallit"
@@ -28,5 +52,8 @@
   [mui/grid {:container true
              :style     {:flex-direction "column"
                          :flex           "1 0 auto"}}
-   [tools]
+   [floating-container {:right 0}
+    [layer-switcher]]
+   [floating-container {:bottom 0 :left 0}
+    [filters]]
    [ol-map/map-outer]])
