63a69f4370e9b48cbaa055b810145fe36175ba85
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-10-15 10:09:34 +0300

    Use webpack to tree shake openlayers for smaller bundle size

--- webapp/src/js/index.js
+++ webapp/src/js/index.js
@@ -1,2 +1,54 @@
-//import * as ol from "ol";
-//window.ol = ol;
+import {Map, View, Overlay} from "ol";
+import {get, fromLonLat} from 'ol/proj';
+import {register} from 'ol/proj/proj4';
+import {Style, Circle, Fill, Stroke, Icon} from 'ol/style';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import {WMTS as WMTSSource, Vector as VectorSource} from 'ol/source';
+import {getTopLeft} from 'ol/extent';
+import {GeoJSON} from 'ol/format';
+import WMTSTileGrid from 'ol/tilegrid/WMTS';
+import {Select} from 'ol/interaction';
+import {pointerMove} from 'ol/events/condition';
+
+window.ol = {
+  Map, View, Overlay,
+  layer: {
+    Tile: TileLayer,
+    Vector: VectorLayer,
+  },
+  source: {
+    WMTS: WMTSSource,
+    Vector: VectorSource,
+  },
+  style: {
+    Style,
+    Circle,
+    Fill,
+    Stroke,
+    Icon,
+  },
+  extent: {
+    getTopLeft,
+  },
+  proj: {
+    get,
+    fromLonLat,
+    proj4: {
+      register,
+    },
+  },
+  tilegrid: {
+    WMTS: WMTSTileGrid,
+  },
+  format: {
+    GeoJSON,
+  },
+  interaction: {
+    Select,
+  },
+  events: {
+    condition: {
+      pointerMove,
+    },
+  },
+};
