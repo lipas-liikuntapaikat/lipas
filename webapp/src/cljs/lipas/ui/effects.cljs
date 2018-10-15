c5bcde1201b4179d36b1ac133e7de6e04c40428e
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-10-15 13:46:51 +0300

    Refactored 'reset scroll' effect under 'effects'

--- webapp/src/cljs/lipas/ui/effects.cljs
+++ webapp/src/cljs/lipas/ui/effects.cljs
@@ -3,6 +3,11 @@
             ["zipcelx"]))
 
 (re-frame/reg-fx
+ ::reset-scroll!
+ (fn  [_]
+   (js/window.scrollTo 0 0)))
+
+(re-frame/reg-fx
  ::download-excel!
  (fn  [config]
    (zipcelx (clj->js config))))
