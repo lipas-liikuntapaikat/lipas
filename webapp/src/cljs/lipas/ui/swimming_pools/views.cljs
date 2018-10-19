add47e9deb5e571839caf4df06c1ca1a950d3091
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-10-19 15:01:39 +0300

    Appends 47e42f9

--- webapp/src/cljs/lipas/ui/swimming_pools/views.cljs
+++ webapp/src/cljs/lipas/ui/swimming_pools/views.cljs
@@ -586,11 +586,12 @@
       [lui/form-card {:title (tr :lipas.visitors/headline)
                       :md    12 :lg 12}
        [sports-site/visitors-view
-        {:tr           tr
-         :lipas-id     lipas-id
-         :editing?     editing?
-         :close        close
-         :display-data display-data}]]
+        {:tr                tr
+         :lipas-id          lipas-id
+         :editing?          editing?
+         :user-can-publish? user-can-publish?
+         :close             close
+         :display-data      display-data}]]
 
       ;;; Energy consumption
       [lui/form-card {:title (tr :lipas.energy-consumption/headline)
