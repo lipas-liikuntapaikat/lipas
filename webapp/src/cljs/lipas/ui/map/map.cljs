790fc54bc9b108cb302dc2e81eae4ff5de3c9612
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-11-07 11:42:50 +0200

    Simplified and unified internal state handling in map component

--- webapp/src/cljs/lipas/ui/map/map.cljs
+++ webapp/src/cljs/lipas/ui/map/map.cljs
@@ -123,7 +123,7 @@
    {:vectors (ol.layer.Vector.
               #js{:source (ol.source.Vector.)})}})
 
-(defn init-map [{:keys [center zoom]}]
+(defn init-map! [{:keys [center zoom]}]
   (let [layers (init-layers)
         view   (ol.View. #js{:center      #js[(:lon center) (:lat center)]
                              :zoom        zoom
@@ -131,19 +131,17 @@
                              :resolutions mml-resolutions
                              :units       "m"})
 
-        overlay (ol.Overlay. #js{:offset #js[-15 0]
-                                 :element
-                                 (js/document.getElementById "popup-anchor")})
+        popup-overlay (ol.Overlay. #js{:offset #js[-15 0]
+                                       :element
+                                       (js/document.getElementById "popup-anchor")})
 
         opts #js {:target   "map"
                   :layers   #js[(-> layers :basemaps :taustakartta)
                                 (-> layers :basemaps :maastokartta)
                                 (-> layers :basemaps :ortokuva)
                                 (-> layers :overlays :vectors)]
-                  :overlays #js[overlay]
+                  :overlays #js[popup-overlay]
                   :view     view}
-
-        lmap (ol.Map. opts)
 
         hover (ol.interaction.Select.
                #js{:layers    [(-> layers :overlays :vectors)]
@@ -152,31 +150,28 @@
 
         select (ol.interaction.Select.
                 #js{:layers #js[(-> layers :overlays :vectors)]
-                    :style  blue-marker-style})]
+                    :style  blue-marker-style})
+
+        lmap (ol.Map. opts)]
 
     (.on hover "select"
          (fn [e]
            (let [coords   (gobj/getValueByKeys e "mapBrowserEvent" "coordinate")
                  selected (aget (gobj/get e "selected") 0)]
-             (.setPosition overlay coords)
+             (.setPosition popup-overlay coords)
              (==> [::events/show-popup
                    (when selected
-                     {:anchor-el (.getElement overlay)
-                      :data      (-> selected
-                                     ->geoJSON-clj)})]))))
-
-    (.addInteraction lmap hover)
+                     {:anchor-el (.getElement popup-overlay)
+                      :data      (-> selected ->geoJSON-clj)})]))))
 
     (.on select "select"
          (fn [e]
            (let [coords   (gobj/getValueByKeys e "mapBrowserEvent" "coordinate")
                  selected (aget (gobj/get e "selected") 0)]
-             (.setPosition overlay coords)
+             (.setPosition popup-overlay coords)
              (==> [::events/show-sports-site
                    (when selected
                      (.get selected "lipas-id"))]))))
-
-    (.addInteraction lmap select)
 
     (.on lmap "moveend"
          (fn [e]
@@ -185,14 +180,20 @@
                  zoom (.getZoom view)]
              (==> [::events/set-view lat lon zoom]))))
 
-    {:lmap   lmap
-     :view   view
-     :interactions
-     {:hover  hover
-      :select select}
-     :layers layers}))
+    {:lmap          lmap
+     :view          view
+     :center        center
+     :zoom          zoom
+     :layers        layers
+     ;; We don't re-create :hover and :select each time when we toggle
+     ;; them because it causes buggy behavior. We keep refs to
+     ;; singleton instances under special :interactions* key in
+     ;; map-ctx where we can find them when they need to be enabled.
+     :interactions* {:select select
+                     :hover  hover}
+     :overlays      {:popup popup-overlay}}))
 
-(defn update-geoms! [{:keys [layers]} geoms]
+(defn update-geoms! [{:keys [layers] :as map-ctx} geoms]
   (let [vectors (-> layers :overlays :vectors)
         source  (.getSource vectors)]
     (.clear source)
@@ -200,30 +201,34 @@
             :let [f (-> g
                         clj->js
                         ->ol-features)]]
-      (.addFeatures source f))))
+      (.addFeatures source f))
+    (assoc map-ctx :geoms geoms)))
 
-(defn set-basemap! [{:keys [layers]} basemap]
+(defn set-basemap! [{:keys [layers] :as map-ctx} basemap]
   (doseq [[k v] (:basemaps layers)
-          :let [visible? (= k basemap)]]
-    (.setVisible v visible?)))
+          :let  [visible? (= k basemap)]]
+    (.setVisible v visible?))
+  map-ctx)
 
-(defn select-feature [{:keys [interactions] :as map-ctx} feature]
+(defn select-feature! [{:keys [interactions] :as map-ctx} feature]
   (let [select (-> interactions :select)]
     (doto (.getFeatures select)
       (.clear)
-      (.push feature)))
-  map-ctx)
+      (.push feature))
+    map-ctx))
 
-(defn select-sports-site! [{:keys [layers interactions] :as map-ctx} lipas-id]
-  (let [layer   (-> layers :overlays :vectors)
-        source  (.getSource layer)
-        fid     (str lipas-id "-0") ; First feature in coll
-        feature (.getFeatureById source fid)]
+(defn find-feature-by-id [{:keys [layers] :as map-ctx} fid]
+  (let [layer  (-> layers :overlays :vectors)
+        source (.getSource layer)]
+    (.getFeatureById source fid)))
+
+(defn select-sports-site! [{:keys [layers] :as map-ctx} lipas-id]
+  (let [feature (find-feature-by-id map-ctx lipas-id) ]
     (if feature
-      (select-feature map-ctx feature)
+      (select-feature! map-ctx feature)
       map-ctx)))
 
-(defn start-editing [{:keys [^js/ol.Map lmap layers interactions]
+(defn start-editing! [{:keys [^js/ol.Map lmap layers interactions]
                               :as   map-ctx} geoJSON-feature on-modifyend]
   (let [layer    (-> layers :overlays :vectors)
         source   (.getSource layer)
@@ -239,20 +244,14 @@
              (on-modifyend (->geoJSON-clj f)))))
     (assoc-in map-ctx [:interactions :modify] modify)))
 
-(defn start-editing-site [{:keys [layers] :as map-ctx} lipas-id on-modifyend]
+(defn start-editing-site! [{:keys [layers] :as map-ctx} lipas-id on-modifyend]
   (let [layer   (-> layers :overlays :vectors)
         source  (.getSource layer)
         fid     (str lipas-id "-0") ; First feature in coll
         feature (.getFeatureById source fid)]
-    (start-editing map-ctx (->geoJSON-clj feature) on-modifyend)))
+    (start-editing! map-ctx (->geoJSON-clj feature) on-modifyend)))
 
-(defn stop-editing [{:keys [^js/ol.Map lmap layers interactions] :as map-ctx}]
-  (let [modify (-> interactions :modify)]
-    (when modify
-      (.removeInteraction lmap modify))
-    (update-in map-ctx [:interactions] dissoc :modify)))
-
-(defn start-drawing [{:keys [^js/ol.Map lmap layers interactions]
+(defn start-drawing! [{:keys [^js/ol.Map lmap layers interactions]
                       :as   map-ctx} geom-type on-draw-end]
   (let [layer  (-> layers :overlays :vectors)
         source (.getSource layer)
@@ -266,82 +265,109 @@
              (on-draw-end (->geoJSON-clj f)))))
     (assoc-in map-ctx [:interactions :draw] draw)))
 
-(defn stop-drawing [{:keys [^js/ol.Map lmap layers interactions] :as map-ctx}]
-  (let [draw   (-> interactions :draw)
-        modify (-> interactions :modify)]
-    (when draw
-      (.removeInteraction lmap draw))
-    (when modify
-      (.removeInteraction lmap modify))
-    (update-in map-ctx [:interactions] dissoc :draw :modify)))
-
-(defn update-view! [{:keys [^js/ol.View view]} {:keys [lon lat zoom]}]
+(defn update-center! [{:keys [^js/ol.View view] :as map-ctx}
+                      {:keys [lon lat] :as center}]
   (.setCenter view #js[lon lat])
-  (.setZoom view zoom))
+  (assoc map-ctx :center center))
 
-(defn clear-interactions [{:keys [^js/ol.Map lmap interactions] :as map-ctx}]
-  (.removeInteraction lmap (-> interactions :select))
-  (.removeInteraction lmap (-> interactions :hover))
-  (when-let [draw (:draw interactions)]
-    (.removeInteraction lmap draw))
-  (when-let [modify (:modify interactions)]
-    (.removeInteraction lmap modify)))
+(defn update-zoom! [{:keys [^js/ol.View view] :as map-ctx} zoom]
+  (.setZoom view zoom)
+  (assoc map-ctx :zoom zoom))
 
-(defn show-feature [{:keys [layers] :as map-ctx} geoJSON-feature]
+(defn show-feature! [{:keys [layers] :as map-ctx} geoJSON-feature]
   (let [vectors (-> layers :overlays :vectors)
         source  (.getSource vectors)
         fs      (-> geoJSON-feature clj->js ->ol-features)]
     (.addFeatures source fs)
     map-ctx))
 
+(defn clear-interactions! [{:keys [^js/ol.Map lmap interactions interactions*]
+                            :as   map-ctx}]
+  ;; Special treatment for 'singleton' interactions*. OpenLayers
+  ;; doesn't treat 'copies' identical to original ones. Therefore we
+  ;; need to pass the original ones explicitly.
+  (doseq [v     (vals (merge interactions interactions*))
+          :when (some? v)]
+    (.removeInteraction lmap v))
+
+  (assoc map-ctx :interactions {}))
+
 ;; Adding new features
 (defn set-adding-mode! [map-ctx mode]
-  (clear-interactions map-ctx)
-  (case (:sub-mode mode)
-    :drawing  (start-drawing map-ctx (:geom-type mode)
-                             (fn [f] (==> [::events/new-geom-drawn f])))
-    :editing  (start-editing map-ctx (:geom mode)
-                             (fn [f] (==> [::events/update-new-geom f])))
-    :finished (show-feature map-ctx (:geom mode))))
+  (let [map-ctx (clear-interactions! map-ctx)]
+    (case (:sub-mode mode)
+      :drawing  (start-drawing! map-ctx (:geom-type mode)
+                                (fn [f] (==> [::events/new-geom-drawn f])))
+      :editing  (start-editing! map-ctx (:geom mode)
+                                (fn [f] (==> [::events/update-new-geom f])))
+      :finished (show-feature! map-ctx (:geom mode)))))
 
-(defn update-adding-mode! [map-ctx mode old-mode]
-  (if (= (:sub-mode mode) (:sub-mode old-mode))
-    map-ctx ;; Noop
-    (set-adding-mode! map-ctx mode)))
+(defn update-adding-mode! [map-ctx mode]
+  (let [old-mode (:mode map-ctx)]
+    (if (= (:sub-mode mode) (:sub-mode old-mode))
+      map-ctx ;; Noop
+      (set-adding-mode! map-ctx mode))))
 
 ;; Editing existing features
-(defn set-editing-mode [map-ctx mode]
-  (clear-interactions map-ctx)
-  (let [lipas-id     (:lipas-id mode)
-        on-modifyend (fn [f] (==> [::events/update-geometries lipas-id f]))]
-    (start-editing-site map-ctx lipas-id on-modifyend)))
+(defn set-editing-mode! [map-ctx mode]
+  (let [map-ctx (clear-interactions! map-ctx)]
+    (let [lipas-id     (:lipas-id mode)
+          on-modifyend (fn [f] (==> [::events/update-geometries lipas-id f]))]
+      (start-editing-site! map-ctx lipas-id on-modifyend))))
 
 (defn update-editing-mode! [map-ctx mode]
   map-ctx)
 
+(defn enable-hover! [{:keys [^js/ol.Map lmap interactions*] :as map-ctx}]
+  (let [hover (:hover interactions*)]
+    (.addInteraction lmap hover)
+    (assoc-in map-ctx [:interactions :hover] hover)))
+
+(defn enable-select! [{:keys [^js/ol.Map lmap interactions*] :as map-ctx}]
+  (let [select (:select interactions*)]
+    (.addInteraction lmap select)
+    (assoc-in map-ctx [:interactions :select] select)))
+
 ;; Browsing and selecting features
 (defn set-default-mode! [{:keys [^js/ol.Map lmap interactions] :as map-ctx} mode]
-  (clear-interactions map-ctx)
-  (.addInteraction lmap (-> interactions :select))
-  (.addInteraction lmap (-> interactions :hover))
-  (when-let [lipas-id (:lipas-id mode)]
-    (select-sports-site! map-ctx lipas-id))
-  map-ctx)
+  (let [map-ctx (-> map-ctx
+                    clear-interactions!
+                    enable-hover!
+                    enable-select!)]
+    (if-let [lipas-id (:lipas-id mode)]
+      (select-sports-site! map-ctx lipas-id)
+      map-ctx)))
 
 (defn update-default-mode! [{:keys [^js/ol.Map lmap interactions] :as map-ctx} mode]
-  (when-let [lipas-id (:lipas-id mode)]
-    (select-sports-site! map-ctx lipas-id))
-  map-ctx)
+  (if-let [lipas-id (:lipas-id mode)]
+    (select-sports-site! map-ctx lipas-id)
+    map-ctx))
+
+(defn set-mode! [map-ctx mode]
+  (let [mode (case (:name mode)
+                   :default (set-default-mode! map-ctx mode)
+                   :editing (set-editing-mode! map-ctx mode)
+                   :adding  (set-adding-mode! map-ctx mode))]
+    (assoc map-ctx :mode mode)))
+
+(defn update-mode! [map-ctx mode]
+  (let [update? (= (-> map-ctx :mode :name) (:name mode))
+        map-ctx (case (:name mode)
+                  :default (if update?
+                             (update-default-mode! map-ctx mode)
+                             (set-default-mode! map-ctx mode))
+                  :editing (if update?
+                             (update-editing-mode! map-ctx mode)
+                             (set-editing-mode! map-ctx mode))
+                  :adding  (if update?
+                             (update-adding-mode! map-ctx mode)
+                             (set-adding-mode! map-ctx mode)))]
+    (assoc map-ctx :mode mode)))
 
 (defn map-inner []
 
-  ;; Internal state atoms
-  (let [map-ctx* (atom nil)
-        geoms*   (atom nil)
-        basemap* (atom nil)
-        center*  (atom nil)
-        zoom*    (atom nil)
-        mode*    (atom nil)]
+  ;; Internal state
+  (let [map-ctx* (atom nil)]
 
     (r/create-class
 
@@ -353,71 +379,36 @@
 
       :component-did-mount
       (fn [comp]
-        (let [opts     (r/props comp)
-              basemap  (:basemap opts)
-              geoms    (:geoms opts)
-              lipas-id (-> opts :site :display-data :lipas-id)
-              center   (-> opts :center)
-              zoom     (-> opts :zoom)
-              mode     (-> opts :mode)
+        (let [opts    (r/props comp)
+              basemap (:basemap opts)
+              geoms   (:geoms opts)
+              center  (-> opts :center)
+              zoom    (-> opts :zoom)
+              mode    (-> opts :mode)
 
-              map-ctx (init-map opts)]
+              map-ctx (-> (init-map! opts)
+                          (update-geoms! geoms)
+                          (set-basemap! basemap)
+                          (set-mode! mode))]
 
-          (reset! map-ctx* map-ctx)
-
-          (reset! basemap* basemap)
-          (reset! zoom* zoom)
-          (reset! center* center)
-          (reset! mode* mode)
-
-          (update-geoms! map-ctx geoms)
-          (set-basemap! map-ctx basemap)
-          (select-sports-site! map-ctx lipas-id)
-
-          (case (:name mode)
-            :default (reset! map-ctx* (set-default-mode! @map-ctx* mode))
-            :editing (reset! map-ctx* (set-editing-mode @map-ctx* mode))
-            :adding  (reset! map-ctx* (set-adding-mode! @map-ctx* mode)))))
+          (reset! map-ctx* map-ctx)))
 
       :component-did-update
       (fn [comp]
-        (let [opts     (r/props comp)
-              geoms    (-> opts :geoms)
-              lipas-id (-> opts :site :display-data :lipas-id)
-              basemap  (-> opts :basemap)
-              center   (-> opts :center)
-              zoom     (-> opts :zoom)
-              mode     (-> opts :mode)]
+        (let [opts    (r/props comp)
+              geoms   (-> opts :geoms)
+              basemap (-> opts :basemap)
+              center  (-> opts :center)
+              zoom    (-> opts :zoom)
+              mode    (-> opts :mode)]
 
-          (when (not= @geoms* geoms)
-            (update-geoms! @map-ctx* geoms)
-            (reset! geoms* geoms))
-
-          (when (not= @basemap* basemap)
-            (set-basemap! @map-ctx* basemap)
-            (reset! basemap* basemap))
-
-          (when (or (not= @zoom* zoom)
-                    (not= @center* center))
-            (update-view! @map-ctx* (merge center {:zoom zoom}))
-            (reset! center* center)
-            (reset! zoom* zoom))
-
-          (when (not= @mode* mode)
-            ;; (prn "Mode changed from!" @mode* "to" mode)
-            (let [update? (= (:name @mode*) (:name mode))
-                  map-ctx (case (:name mode)
-                            :default (if update?
-                                       (update-default-mode! @map-ctx* mode)
-                                       (set-default-mode! @map-ctx* mode))
-                            :editing (if update?
-                                       (update-editing-mode! @map-ctx* mode)
-                                       (set-editing-mode @map-ctx* mode))
-                            :adding  (if update?
-                                       (update-adding-mode! @map-ctx* mode @mode*)
-                                       (set-adding-mode! @map-ctx* mode)))]
-              (reset! map-ctx* map-ctx)
-              (reset! mode* mode)))))
+          (cond-> @map-ctx*
+            (not= (:geoms @map-ctx*) geoms)     (update-geoms! geoms)
+            (not= (:basemap @map-ctx*) basemap) (set-basemap! basemap)
+            (not= (:center @map-ctx*) center)   (update-center! center)
+            (not= (:zoom @map-ctx*) zoom)       (update-zoom! zoom)
+            (not= (:mode @map-ctx*) mode)       (update-mode! mode)
+            true                                (as-> $ (reset! map-ctx* $)))))
 
       :display-name "map-inner"})))
 
@@ -430,7 +421,6 @@
         basemap (re-frame/subscribe [::subs/basemap])
         center  (re-frame/subscribe [::subs/center])
         zoom    (re-frame/subscribe [::subs/zoom])
-        site    (re-frame/subscribe [::subs/selected-sports-site])
         mode    (re-frame/subscribe [::subs/mode])]
     (fn []
       [map-inner
@@ -438,5 +428,4 @@
         :basemap @basemap
         :center  @center
         :zoom    @zoom
-        :site    @site
         :mode    @mode}])))
