6ce4a15976deaa631e5e7fa8bc4bc2782795b8a8
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-12-31 17:35:35 +0200

    Refactored migration code
    - removed legacy portal related migration code
    
    - created separate namespace for old-lipas integrations

---
+++ webapp/src/clj/lipas/integration/old_lipas/api.clj
@@ -0,0 +1,67 @@
+(ns lipas.integration.old-lipas.api
+  (:require
+   [cheshire.core :as json]
+   [clj-http.client :as client]
+   [clojure.string :as string]
+   [environ.core :refer [env]]
+   [ring.util.codec :as codec]))
+
+(def url (:old-lipas-url env))
+
+(def fields
+  ["properties" "schoolUse" "email" "type.name" "location.sportsPlaces"
+   "renovationYears" "admin" "www" "location.geometries" "name"
+   "type.typeCode" "location.locationId" "freeUse" "location.city.name"
+   "lastModified" "location.postalCode" "location.postalOffice"
+   "location.city.cityCode" "phoneNumber" "location.neighborhood"
+   "owner" "location.address"])
+
+(defn get [lipas-id]
+  (let [url (url "/api/sports-places/" lipas-id)]
+    (-> url slurp (json/decode true))))
+
+(defn- query [results url {:keys [page pageSize] :as params
+                           :or   {page 1 pageSize 100}}]
+  (let [params (assoc params :page page :pageSize pageSize)
+        url*   (str url "?" (codec/form-encode params))]
+    (if-let [data (-> url* slurp (json/decode true) not-empty)]
+      (recur (into results data) url (update params :page inc))
+      results)))
+
+(defn query-changed [since]
+  (let [url    (str url "/api/sports-places")
+        params {:modifiedAfter since
+                :fields fields}]
+    (query [] url params)))
+
+(defn query-timestamps [lipas-ids]
+  (let [url    (str url "/api/sports-places")
+        params {:searchString (string/join "|" lipas-ids)
+                :fields       ["lastModified"]}]
+    (->> (query [] url params)
+         (filter (comp (set lipas-ids) :sportsPlaceId)))))
+
+(defn get-ice-stadiums []
+  (let [url    (str url "/api/sports-places")
+        params {:typeCodes [2520 2510]
+                :fields    fields}]
+    (query [] url params)))
+
+(defn get-swimming-pools []
+  (let [url    (str url "/api/sports-places")
+        params {:typeCodes [3110 3130]
+                :fields    fields}]
+    (query [] url params)))
+
+(defn post-integration-doc! [doc]
+  (let [params {:basic-auth
+                [(:old-lipas-user env) (:old-lipas-pass env)]
+                :content-type     :json
+                :accept           :json
+                :socket-timeout   10000
+                :conn-timeout     10000
+                :throw-exceptions true
+                :body (json/encode doc)}
+
+        url (str url "/api/integration/doc")]
+    (client/post url params)))
