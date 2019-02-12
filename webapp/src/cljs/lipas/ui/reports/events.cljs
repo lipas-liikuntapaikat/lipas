7726113621e28e371f36131c4aaee9b02a39658d
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2019-02-12 12:24:56 +0200

    Fixed excel reports to have their columns in sensible order

--- webapp/src/cljs/lipas/ui/reports/events.cljs
+++ webapp/src/cljs/lipas/ui/reports/events.cljs
@@ -2,6 +2,7 @@
   (:require
    [ajax.core :as ajax]
    [ajax.protocols :as ajaxp]
+   [clojure.set :as cset]
    [lipas.utils :as cutils]
    [re-frame.core :as re-frame]))
 
@@ -17,22 +18,54 @@
      (update-in db [:reports :selected-fields] (comp set into) v)
      (assoc-in db [:reports :selected-fields] v))))
 
+(def basic-fields
+  ["lipas-id"
+   "name"
+   "marketing-name"
+   "type.type-code"
+   "type.type-name"
+   "location.city.city-code"
+   "location.city.city-name"
+   "construction-year"
+   "admin"
+   "owner"
+   "renovation-years"
+   "www"
+   "phone-number"
+   "email"
+   "location.address"
+   "location.city.neighborhood"
+   "location.postal-code"
+   "location.postal-office"
+   "comment"])
+
+(defn sort-headers
+  "Returns vector of headers where basic-fields are in predefined order
+  and rest in natural order."
+  [fields]
+  (let [basic (filterv #(some #{%} fields) basic-fields)
+        others (cset/difference (set fields) (set basic))]
+    (into [] cat
+          [basic
+           (sort others)])))
+
 (re-frame/reg-event-fx
  ::create-report
  (fn [{:keys [db]} [_ query fields]]
-   {:http-xhrio
-    {:method          :post
-     :uri             (str (:backend-url db) "/actions/create-sports-sites-report")
-     :params          {:search-query query
-                       :fields       fields}
-     :format          (ajax/json-request-format)
-     :response-format {:type         :blob
-                       :content-type (-> cutils/content-type :xlsx)
-                       :description  (-> cutils/content-type :xlsx)
-                       :read         ajaxp/-body}
-     :on-success      [::report-success]
-     :on-failure      [::report-failure]}
-    :db (assoc-in db [:reports :downloading?] true)}))
+   (let [fields (sort-headers fields)]
+     {:http-xhrio
+      {:method          :post
+       :uri             (str (:backend-url db) "/actions/create-sports-sites-report")
+       :params          {:search-query query
+                         :fields       fields}
+       :format          (ajax/json-request-format)
+       :response-format {:type         :blob
+                         :content-type (-> cutils/content-type :xlsx)
+                         :description  (-> cutils/content-type :xlsx)
+                         :read         ajaxp/-body}
+       :on-success      [::report-success]
+       :on-failure      [::report-failure]}
+      :db (assoc-in db [:reports :downloading?] true)})))
 
 (re-frame/reg-event-fx
  ::report-success
