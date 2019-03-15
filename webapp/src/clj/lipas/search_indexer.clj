5724b2360d5c0310768ba65000783d413e160cc0
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2019-03-15 11:09:23 +0200

    Added utils for indexing data for analytics

--- webapp/src/clj/lipas/search_indexer.clj
+++ webapp/src/clj/lipas/search_indexer.clj
@@ -1,20 +1,26 @@
 (ns lipas.search-indexer
-  (:require [lipas.backend.config :as config]
-            [lipas.backend.core :as core]
-            [lipas.backend.search :as search]
-            [lipas.backend.system :as backend]
-            [lipas.data.cities :as cities]
-            [lipas.data.types :as types]
-            [lipas.utils :as utils]
-            [taoensso.timbre :as log]))
+  (:require
+   [lipas.backend.config :as config]
+   [lipas.backend.core :as core]
+   [lipas.backend.search :as search]
+   [lipas.backend.system :as backend]
+   [lipas.data.cities :as cities]
+   [lipas.data.types :as types]
+   [lipas.utils :as utils]
+   [taoensso.timbre :as log]))
 
 (def cities (utils/index-by :city-code cities/all))
 (def types types/all)
 
-(defn index-all!
-  ([db search user idx-name types]
-   (index-all! db search user idx-name types []))
-  ([db search user idx-name types futures]
+(defn- wait-all [futures]
+  (log/info "Waiting for indexing requests to get processed...")
+  (doseq [f futures]
+    (deref f)))
+
+(defn index-search!
+  ([db search idx-name types]
+   (index-search! db search idx-name types []))
+  ([db search idx-name types futures]
    (let [type-code (first types)]
      (log/info "Starting to re-index type" type-code)
      (if type-code
@@ -24,28 +30,64 @@
             (search/->bulk idx-name :lipas-id)
             (search/bulk-index! search)
             (conj futures)
-            (recur db search user idx-name (rest types)))
-       (do
-         (log/info "Waiting for indexing requests to get processed...")
-         (doseq [f futures]
-          (deref f)))))))
+            (recur db search idx-name (rest types)))
+       (wait-all futures)))))
+
+(defn enrich-for-analytics
+  [users {:keys [id document author_id status created_at]}]
+  (-> document
+      core/enrich
+      (assoc :id id
+             :doc-status status
+             :author (users author_id)
+             :created-at created_at)))
+
+(defn get-users [db]
+  (->> (core/get-users db)
+       (map #(select-keys % [:id :email :permissions :status]))
+       (utils/index-by :id)))
+
+(defn index-analytics!
+  ([db search idx-name types users]
+   (index-analytics! db search idx-name types users []))
+  ([db search idx-name types users futures]
+   (let [type-code  (first types)
+         query-opts {:raw? true :revs "all"}]
+     (log/info "Starting to re-index type" type-code)
+     (if type-code
+       (->> (core/get-sports-sites-by-type-code db type-code query-opts)
+            (map (partial enrich-for-analytics users))
+            (search/->bulk idx-name :id)
+            (search/bulk-index! search)
+            (conj futures)
+            (recur db search idx-name (rest types) users))
+       (wait-all futures)))))
 
 (defn -main [& args]
-  (let [config (select-keys config/default-config [:db :search])
+  (let [mode   (case (first args)
+                 "--analytics" "analytics"
+                 "search")
+        config (select-keys config/default-config [:db :search])
         system (backend/start-system! config)
         db     (:db system)
-        search (:search system)
-        user   (core/get-user db "import@lipas.fi")]
+        search (:search system)]
     (try
-      (let [idx-name (search/gen-idx-name)
+      (let [idx-name (str mode "-" (search/gen-idx-name))
             mappings (:sports-sites search/mappings)
             types    (keys types/all)
-            alias    "sports_sites_current"]
+            alias    (case mode
+                       "search"    "sports_sites_current"
+                       "analytics" "analytics")]
         (log/info "Starting to re-index types" types)
         (search/create-index! search idx-name mappings)
         (log/info "Created index" idx-name)
         (log/info "Starting to index data...")
-        (index-all! db search user idx-name types)
+
+        (case mode
+          "search"    (index-search! db search idx-name types)
+          "analytics" (let [users (get-users db)]
+                        (index-analytics! db search idx-name types users)))
+
         (log/info "Indexing data done!")
         (log/info "Swapping alias" alias "to point to index" idx-name)
         (let [old-idxs (search/swap-alias! search {:new-idx idx-name :alias alias})]
@@ -57,9 +99,11 @@
 
 (comment
   (-main)
+  (-main "--analytics")
   (def config (select-keys config/default-config [:db :search]))
   (def system (backend/start-system! config))
   (def db (:db system))
+  (get-user-data db)
   (def search (:search system))
   (def user (core/get-user db "import@lipas.fi"))
   (search/delete-index! search "2018-*")
