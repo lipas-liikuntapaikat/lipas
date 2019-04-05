1fdb154d38d1c9cf5d34cb6b5e0ae413dbc4cf69
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2019-04-05 09:39:17 +0300

    Added custom tooltip and legend to age-structure chart

--- webapp/src/cljs/lipas/ui/charts.cljs
+++ webapp/src/cljs/lipas/ui/charts.cljs
@@ -4,7 +4,7 @@
    [clojure.set :refer [rename-keys map-invert]]
    [goog.object :as gobj]
    [lipas.ui.mui :as mui]
-   [lipas.ui.components.tables :as tables]
+   [lipas.ui.components.misc :as misc]
    [reagent.core :as r]))
 
 ;; Tip: enable rainbow-mode in emacs.
@@ -32,6 +32,7 @@
    "other"                  mui/gray1
    "city"                   "#D35400"
    "city-main-owner"        "#DC7633"
+   "municipal-consortium"   "#dc9b33"
    "registered-association" "#2980B9"
    "company-ltd"            "#8E44AD"
    "state"                  "#1ABC9C"
@@ -45,8 +46,7 @@
    "city-other"              "#873600"
    "private-association"     "#2980B9"
    "private-company"         "#8E44AD"
-   "private-foundation"      "#27AE60"
-   })
+   "private-foundation"      "#27AE60"})
 
 (def font-styles
   {:font-family "lato"})
@@ -149,53 +149,32 @@
               :dataKey k2
               :stroke  (get colors (get lookup k1))}])))]))
 
-
-(defn age-structure-chart [{:keys [data labels]}]
-  (let [lookup (map-invert labels)
-        data   (->> data (map #(rename-keys % labels)))]
-    [:> rc/ResponsiveContainer {:width "100%" :height 300}
-     (into
-      [:> rc/BarChart {:data data}
-       [:> rc/Legend {:wrapperStyle font-styles}]
-       [:> rc/Tooltip tooltip-styles]
-       [:> rc/YAxis
-        {:tick font-styles
-         :label
-         {:value    (:y-axis labels)
-          :angle    -90
-          :offset   10
-          :position "insideBottomLeft"}}]
-       [:> rc/XAxis {:dataKey :construction-year :tick font-styles}]]
-      (for [k (->> data (mapcat keys) set (remove #(= :construction-year %)) sort)]
-        [:> rc/Bar
-         {:stackId "a"
-          :dataKey k
-          :label   false
-          :fill    (get colors (get lookup k))}]))]))
-
-(defn custom-tooltip [labels props]
+(defn tooltip
+  "`payload-fn` should return a map with
+  keys :label :value (:icon :color)."
+  [labels payload-fn props]
   (let [label   (gobj/get props "label")
-        payload (gobj/get props "payload")
-        entry   (-> payload
-                    first
-                    (gobj/get "payload")
-                    (js->clj :keywordize-keys true))]
+        payload (gobj/get props "payload")]
     (r/as-element
      [mui/paper {:style {:padding "0.5em"}}
-      [mui/typography {:variant "subtitle2"}
+
+      ;; Tooltip header
+      [mui/typography
+       {:variant "body2" :align "center" :style {:margin-bottom "0.25em"}}
        label]
+
+      ;; Content table
       [mui/table {:style {:width "350"} :padding "dense"}
-       (->> entry
-            (reduce
-             (fn [res [k v]]
-               (if-let [label (labels k)]
-                 (conj res {:label label :value v})
-                 res))
-             [])
+       (->> payload
+            payload-fn
             (sort-by :label)
             (map
-             (fn [{:keys [label value]}]
-               [mui/table-row
+             (fn [{:keys [label value icon color]}]
+               [mui/table-row {:style {:height "24px"}}
+                (when icon
+                  [mui/table-cell {:padding "checkbox"}
+                   [mui/icon {:style {:color color}}
+                    icon]])
                 [mui/table-cell
                  [mui/typography {:variant "caption"}
                   label]]
@@ -203,6 +182,75 @@
                  [mui/typography
                   value]]]))
             (into [mui/table-body]))]])))
+
+(defn age-structure-tooltip [labels props]
+  (let [payload-fn (fn [payload]
+                     (->> payload
+                          (map
+                           (fn [obj]
+                             {:label (labels (gobj/get obj "name"))
+                              :color (gobj/get obj "fill")
+                              :icon  "label"
+                              :value (gobj/get obj "value")}))))]
+    (tooltip labels payload-fn props)))
+
+(def legend-icons
+  {"rect" "label"})
+
+(defn legend [labels props]
+  (let [payload (gobj/get props "payload")]
+    (r/as-element
+     (->> payload
+          (map
+           (fn [obj]
+             {:label (or (labels (gobj/get obj "value"))
+                         (labels (keyword (gobj/get obj "value"))))
+              :color (gobj/get obj "color")
+              :type  (gobj/get obj "type")}))
+          (sort-by :label)
+          (map
+           (fn [{:keys [label color type]}]
+             [mui/grid {:item true}
+              [misc/icon-text2
+               {:icon (legend-icons type) :icon-color color :text label}]]))
+          (into
+           [mui/grid {:container true :justify "center"}])))))
+
+(defn age-structure-chart [{:keys [data labels]}]
+  [:> rc/ResponsiveContainer {:width "100%" :height 300}
+   (into
+    [:> rc/BarChart {:data data}
+     [:> rc/Legend {:content (partial legend labels)}]
+     [:> rc/Tooltip {:content (partial age-structure-tooltip labels)}]
+     [:> rc/YAxis
+      {:tick font-styles
+       :label
+       {:value    (:y-axis labels)
+        :angle    -90
+        :offset   10
+        :position "insideBottomLeft"}}]
+     [:> rc/XAxis {:dataKey :construction-year :tick font-styles}]]
+    (for [k (->> data (mapcat keys) set (remove #(= :construction-year %)) sort)]
+      [:> rc/Bar
+       {:stackId "a"
+        :dataKey k
+        :label   false
+        :fill    (get colors k)}]))])
+
+(defn sports-stats-tooltip [labels props]
+  (let [payload-fn (fn [payload]
+                     (let [entry (-> payload
+                                     first
+                                     (gobj/get "payload")
+                                     (js->clj :keywordize-keys true))]
+                       (->> entry
+                            (reduce
+                             (fn [res [k v]]
+                               (if-let [label (labels k)]
+                                 (conj res {:label label :value v})
+                                 res))
+                             []))))]
+    (tooltip labels payload-fn props)))
 
 (defn sports-stats-chart [{:keys [data labels metric grouping]}]
   (let [;;data       (->> data (map #(rename-keys % labels)))
@@ -212,8 +260,8 @@
                      :type-name)]
     [:> rc/ResponsiveContainer {:width "100%" :height (+ 60 (* 30 (count data)))}
      [:> rc/BarChart {:data data :layout "vertical" :margin margin}
-      [:> rc/Legend {:wrapperStyle font-styles}]
-      [:> rc/Tooltip {:content (partial custom-tooltip labels)}]
+      [:> rc/Legend {:content (partial legend labels)}]
+      [:> rc/Tooltip {:content (partial sports-stats-tooltip labels)}]
       [:> rc/XAxis {:tick font-styles :type "number"}]
       [:> rc/YAxis {:dataKey y-axis-key :type "category" :tick font-styles}]
       [:> rc/Bar {:dataKey (keyword metric) :fill "orange"}
