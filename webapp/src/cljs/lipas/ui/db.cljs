b1e49ba546b843d1beb32164c061d72a076743a8
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2019-05-09 18:11:04 +0300

    Added "ranking" version of finance stats chart

--- webapp/src/cljs/lipas/ui/db.cljs
+++ webapp/src/cljs/lipas/ui/db.cljs
@@ -159,17 +159,19 @@
      :selected-metric   "sites-count"
      :selected-view     "chart"}
     :finance
-    {:selected-view         "chart"
-     :selected-cities       [] ; whole country
-     :selected-year         2017
-     :units                 reports/stats-units
-     :selected-unit         "euros-per-capita"
-     :city-services         reports/city-services
-     :selected-city-service "sports-services"
-     :groupings             reports/finance-stats-groupings
-     :selected-grouping     "avi"
-     :metrics               reports/stats-metrics
-     :selected-metrics      ["net-costs" "investments"]}}
+    {:selected-view           "chart"
+     :selected-cities         [] ; whole country
+     :selected-year           2017
+     :units                   reports/stats-units
+     :selected-unit           "euros-per-capita"
+     :city-services           reports/city-services
+     :selected-city-service   "sports-services"
+     :groupings               reports/finance-stats-groupings
+     :selected-grouping       "avi"
+     :metrics                 reports/stats-metrics
+     :selected-metrics        ["net-costs" "investments"]
+     :selected-ranking-metric "net-costs"
+     :chart-type              "comparison"}}
 
    ;; Map
    :map
