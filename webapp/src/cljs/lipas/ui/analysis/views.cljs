63bf60e2fb120b22c911c49c830985f5578c840e
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2022-08-13 22:11:28 +0300

    Rename old "nalysis tool" to "Accessibility tool"

--- webapp/src/cljs/lipas/ui/analysis/views.cljs
+++ webapp/src/cljs/lipas/ui/analysis/views.cljs
@@ -11,22 +11,22 @@
 (defn view []
   (let [selected-tool (<== [::subs/selected-tool])]
     [mui/grid {:container true :style {:padding "1em"}}
-     
+
      ;; Header and close button
-     [mui/grid {:item true :container true :justify "space-between"}      
-      [mui/grid {:item true :xs 10}       
+     [mui/grid {:item true :container true :justify "space-between"}
+      [mui/grid {:item true :xs 10}
        [mui/tabs
         {:value selected-tool
          :on-change #(==> [::events/select-tool %2])
          :variant "fullWidth"
          :centered true
          :indicatorColor "primary"}
-        [mui/tab {:value "reachability" :label "Analyysi (beta)"}]
+        [mui/tab {:value "reachability" :label "Saavutettavuus (beta)"}]
         [mui/tab {:value "diversity" :label "Monipuolisuus (beta)"}]]]
       [mui/grid {:item true}
        [mui/icon-button {:on-click #(==> [::map-events/hide-analysis])}
         [mui/icon "close"]]]]
-     
+
      [mui/grid {:item true :xs 12}
       (condp = selected-tool
         "reachability" [reachability/analysis-view]
