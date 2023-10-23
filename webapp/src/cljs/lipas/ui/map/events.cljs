d710ee903c3511ca936fc21ed3444efa28255679
Tuomas-Matti Soikkeli <tuomas-matti.soikkeli@siili.com>
2023-10-23 11:19:46 +0300

    Add comments

--- webapp/src/cljs/lipas/ui/map/events.cljs
+++ webapp/src/cljs/lipas/ui/map/events.cljs
@@ -322,7 +322,8 @@
 (re-frame/reg-event-fx
  ::map-clicked
  (fn [event]
-   (-> event .-target .getTargetElement .focus)
+   ;; Set browser focus to Map element to enable keyboard pan/zoom
+   (-> event .-target .getTargetElement .focus) 
    {:dispatch [::hide-address]}))
 
 (re-frame/reg-event-fx
