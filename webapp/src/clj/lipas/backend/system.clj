af80a9cd9ac8c50ca285c868c5d4ecee8cfd753f
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2024-07-28 14:56:19 +0300

    First batch of type-code changes

--- webapp/src/clj/lipas/backend/system.clj
+++ webapp/src/clj/lipas/backend/system.clj
@@ -71,6 +71,18 @@
 (defmethod ig/halt-key! :lipas/aws [_ m]
   )
 
+(defmethod ig/init-key :open-ai [_ config]
+  )
+
+(defmethod ig/halt-key! :open-ai [_ m]
+  )
+
+(defmethod ig/init-key :ptv [_ config]
+  )
+
+(defmethod ig/halt-key! :ptv [_ m]
+  )
+
 (defn mask [_s]
   "[secret]")
 
