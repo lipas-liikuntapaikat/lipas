5582d4eba164aa8cba10f91dd70c669b858b2c69
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2024-08-04 16:25:50 +0300

    Conform to newer integrant spec

--- webapp/src/clj/lipas/backend/system.clj
+++ webapp/src/clj/lipas/backend/system.clj
@@ -71,6 +71,19 @@
 (defmethod ig/halt-key! :lipas/aws [_ m]
   )
 
+(defmethod ig/init-key :lipas/open-ai [_ config]
+  )
+
+(defmethod ig/halt-key! :lipas/open-ai [_ m]
+  )
+
+(defmethod ig/init-key :lipas/ptv [_ config]
+  )
+
+(defmethod ig/halt-key! :lipas/ptv [_ m]
+  )
+
+
 (defn mask [_s]
   "[secret]")
 
