6892998cb48abf4efdc1526d19f0fefc3f26fc1a
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2024-08-14 20:59:01 +0300

    Scrap

---
+++ webapp/src/clj/lipas/backend/ptv.clj
@@ -0,0 +1,49 @@
+(ns lipas.backend.ptv
+  (:require
+   [lipas.data.types :as types]
+   [cheshire.core :as json]
+   [clj-http.client :as client]))
+
+;; Exploring PTV prod data
+
+(def api-url "https://api.palvelutietovaranto.suomi.fi/api/v11")
+
+(def headers {:Content-Type  "application/json"})
+
+(defn get-services-by-class
+  [class-uri page]
+  (-> (client/get (str api-url "/Service/serviceClass")
+                  {:headers headers
+                   :query-params
+                   {:uri  class-uri
+                    :page page}})
+        :body
+        (json/decode keyword)))
+
+(defn get-services-channels-by-type
+  [type page]
+  (-> (client/get (str api-url "/Service/serviceClass/" type)
+                  {:headers headers
+                   :query-params
+                   {:page page}})
+        :body
+        (json/decode keyword)))
+
+(comment
+
+  (->> types/main-categories
+       vals
+       (mapcat (comp :service-classes :ptv))
+       distinct)
+
+  ["http://uri.suomi.fi/codelist/ptv/ptvserclass2/code/P27.2"
+   "http://uri.suomi.fi/codelist/ptv/ptvserclass2/code/P27.1"]
+
+  (def p1 (get-services-by-class "http://uri.suomi.fi/codelist/ptv/ptvserclass2/code/P27.2" 1))
+  (def p2 (get-services-by-class "http://uri.suomi.fi/codelist/ptv/ptvserclass2/code/P27.2" 2))
+
+
+
+
+
+  )
