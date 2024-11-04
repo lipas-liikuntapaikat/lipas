95e88255be77155c9636c099de9eef3f0736a10c
Tuomas-Matti Soikkeli <tuomas-matti.soikkeli@siili.com>
2024-11-04 17:29:07 +0200

    Add properties to sports-place-type by type code

--- webapp/src/clj/lipas/backend/legacy/api.clj
+++ webapp/src/clj/lipas/backend/legacy/api.clj
@@ -4,41 +4,45 @@
    [lipas.data.types :as types]
    [lipas.utils :as utils]))
 
+(def keys-vec [:type-code :name :description :geometry-type :sub-category])
+
 (defn fill-properties [m]
   (reduce (fn [acc k] (assoc-in acc [:props k] (prop-types/all k)))
           m
           (-> m :props keys)))
 
 
-(defn localize-props [m locale prop-keys]
+(defn localize-properties [m locale]
   (reduce (fn [acc k]
             (-> acc
                 (update-in [:props k :description] locale)
-                (update-in [:props k :name] locale)))
-          m
-          prop-keys))
+                (update-in [:props k :name] locale))) m (-> m :props keys)))
 
 (defn- localize [m lang]
-  (let [prop-keys (-> m :props keys)]
-   (-> m
-       (update :name lang)
-       (update :description lang)
-       (localize-props lang prop-keys))))
+  (-> m
+      (update :name lang)
+      (update :description lang)))
+
+
+(defn- ->legacy-api-with-properties [m lang]
+  (-> m
+      (fill-properties)
+      (select-keys (conj keys-vec :props))
+      (update :props #(-> % (dissoc :schoolUse? :freeUse?)))
+      (localize-properties lang)
+      (localize lang)
+      utils/->camel-case-keywords))
 
 (defn- ->legacy-api [m lang]
   (-> m
-      (fill-properties)
+      (select-keys keys-vec)
       (localize lang)
-      (update :name lang)
-      (update :description lang)
       utils/->camel-case-keywords))
 
-
-
-(prop-types/all :track-length-m)
 (defn sports-place-types [lang]
-  (->> (vals types/all)
-       (map #(->legacy-api % lang))))
+  (->>  (vals types/all)
+        (map #(->legacy-api % lang))))
 
 (defn sports-place-by-type-code [lang type-code]
-  (->legacy-api (types/all type-code) lang))
+  (-> (types/all type-code)
+      (->legacy-api-with-properties lang)))
