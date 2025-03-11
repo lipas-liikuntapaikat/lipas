0c42c279c6bb32e1caf5df096fb3a826539c0d09
Tuomas-Matti Soikkeli <tuomas-matti.soikkeli@siili.com>
2025-03-11 16:46:43 +0200

    I'm lost, help valtteri

--- webapp/dev/user.clj
+++ webapp/dev/user.clj
@@ -44,6 +44,10 @@
   []
   ((requiring-resolve 'lipas.search-indexer/main) (db) (search) "search"))
 
+(defn reindex-legacy-search!
+  []
+  ((requiring-resolve 'lipas.search-indexer/main) (db) (search) "legacy"))
+
 (defn reindex-analytics!
   []
   ((requiring-resolve 'lipas.search-indexer/main) (db) (search) "analytics"))
@@ -62,6 +66,7 @@
   (reset)
 
   (reindex-search!)
+  (reindex-legacy-search!)
   (reindex-analytics!)
   (reset-admin-password! "kissa13")
   (reset-password! "valtteri.harmainen@gmail.com" "kissa13")
