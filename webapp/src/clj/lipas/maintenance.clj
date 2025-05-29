8183b1a4634a5a22709223d378c8384673d35ce4
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2025-05-29 13:53:37 +0300

    chore: complete Leiningen to deps.edn migration cleanup
    - Remove webapp/project.clj (Leiningen project file)
    - Remove webapp/dev/legacy_dev.clj (legacy lein ring dev file)
    - Update bb.edn tasks to use clojure commands instead of lein
    - Update maintenance.clj usage message to use clojure command
    - Clean up .gitignore to remove Leiningen-specific entries
    
    All Leiningen artifacts have been removed. Project now fully uses
    deps.edn + tools.build + Babashka stack.

--- webapp/src/clj/lipas/maintenance.clj
+++ webapp/src/clj/lipas/maintenance.clj
@@ -29,7 +29,7 @@
 (defn merge-types
   [db search ptv user type-code-from type-code-to]
   (let [types* (merge old-types/all types/all)]
-    (assert (contains? types*  type-code-from))
+    (assert (contains? types* type-code-from))
     (assert (contains? types/all type-code-to))
     (assert (= (get-in types* [type-code-from :geometry-type])
                (get-in types/all [type-code-to :geometry-type]))))
@@ -43,7 +43,7 @@
 (defn duplicate-point->area-draft
   [db search ptv user type-code-from type-code-to]
   (let [types* (merge old-types/all types/all)]
-    (assert (contains? types*  type-code-from))
+    (assert (contains? types* type-code-from))
     (assert (contains? types/all type-code-to))
     (assert (= "Point" (get-in types* [type-code-from :geometry-type])))
     (assert (= "Polygon" (get-in types/all [type-code-to :geometry-type]))))
@@ -96,23 +96,23 @@
    (fn [res {:keys [city-code stats]}]
      (into res
            (for [[year data] stats
-                 :let        [youth (-> data :services :youth-services)
-                              sports (-> data :services :sports-services)
-                              city (all-cities city-code)
-                              province (cities/provinces (:province-id city))
-                              avi (cities/avi-areas (:avi-id city))
-                              popl (:population data)]]
+                 :let [youth (-> data :services :youth-services)
+                       sports (-> data :services :sports-services)
+                       city (all-cities city-code)
+                       province (cities/provinces (:province-id city))
+                       avi (cities/avi-areas (:avi-id city))
+                       popl (:population data)]]
              (merge
               (select-keys city [:province-id :avi-id])
-              {:id         (str city-code "-" year)
-               :date       (str year "-01-01")
-               :city-code  city-code
-               :year       year
+              {:id (str city-code "-" year)
+               :date (str year "-01-01")
+               :city-code city-code
+               :year year
                :population popl
                :search_meta
-               {:city-name     (:name city)
+               {:city-name (:name city)
                 :province-name (:name province)
-                :avi-name      (:name avi)}}
+                :avi-name (:name avi)}}
               (utils/->prefix-map youth "youth-services-")
               (utils/->prefix-map sports "sports-services-")
               (-> youth
@@ -131,7 +131,7 @@
        :province-id (-> m :city-code all-cities :province-id)
        :avi-id (-> m :city-code all-cities :avi-id))
       (cond->
-          (->> m :type-codes (remove nil?) empty?) (assoc :type-codes [-1]))
+       (->> m :type-codes (remove nil?) empty?) (assoc :type-codes [-1]))
       (dissoc :city-name)))
 
 ;; There are no sensible primary keys in the data so we purge and
@@ -141,7 +141,7 @@
   'subsidy'."
   [{:keys [db search]}]
   (let [{:keys [indices client]} search
-        es-index                 (get-in indices [:report :subsidies])]
+        es-index (get-in indices [:report :subsidies])]
     (log/info "Deleting index" es-index)
     (search/delete-index! client es-index)
     (log/info "Deleted" es-index)
@@ -157,29 +157,29 @@
 (def city-lookup (utils/index-by (comp :fi :name) :city-code cities/all))
 
 (def subsidy-csv-headers
-    {"Saajataho, luokiteltu (Lipas-luokitus omistaja)" :owner
-     "Liikuntapaikan nimi"                             :target
-     "Avustuksen saaja"                                :receiver-name
-     "Kunta myöntövuonna"                              :city-name
-     "Myöntövuosi"                                     :year
-     "Tyyppikoodi (numero)"                            :type-codes
-     "Avustuksen myöntäjä"                             :issuer
-     "Lipas-ID"                                        :lipas-ids
-     "Myönnetty avustus tuhatta e"                     :amount
-     "Avustuksen selite"                               :description})
+  {"Saajataho, luokiteltu (Lipas-luokitus omistaja)" :owner
+   "Liikuntapaikan nimi" :target
+   "Avustuksen saaja" :receiver-name
+   "Kunta myöntövuonna" :city-name
+   "Myöntövuosi" :year
+   "Tyyppikoodi (numero)" :type-codes
+   "Avustuksen myöntäjä" :issuer
+   "Lipas-ID" :lipas-ids
+   "Myönnetty avustus tuhatta e" :amount
+   "Avustuksen selite" :description})
 
 (defn ->subsidy-db-entry [m]
-    (-> m
-        (assoc :city-code (-> m :city-name city-lookup))
-        (update :type-codes #(-> % (str/split #";") (->> (mapv utils/->int)) set))
-        (update :year utils/->int)
-        (update :lipas-ids #(-> % (str/split #";") (->> (mapv utils/->int) (remove nil?))))
-        (update :owner owner-lookup)
-        (update :issuer #(condp = %
-                           "ELY" "AVI"
-                           "OPM" "OKM"
-                           %))
-        (update :amount utils/->number)))
+  (-> m
+      (assoc :city-code (-> m :city-name city-lookup))
+      (update :type-codes #(-> % (str/split #";") (->> (mapv utils/->int)) set))
+      (update :year utils/->int)
+      (update :lipas-ids #(-> % (str/split #";") (->> (mapv utils/->int) (remove nil?))))
+      (update :owner owner-lookup)
+      (update :issuer #(condp = %
+                         "ELY" "AVI"
+                         "OPM" "OKM"
+                         %))
+      (update :amount utils/->number)))
 
 (def subsidy-db-entry-schema
   [:map
@@ -230,7 +230,7 @@
   "Re-indexes documents from db table 'city' into ES."
   [{:keys [db search]}]
   (let [{:keys [indices client]} search
-        es-index                 (get-in indices [:report :city-stats])]
+        es-index (get-in indices [:report :city-stats])]
     (log/info "Starting to index city finance data to" es-index)
     (->> (core/get-cities db :no-cache)
          ->city-finance-entries
@@ -245,86 +245,86 @@
 (defn ->city-stats-map
   [csv-data year]
   (reduce
-     (fn [res m]
-       (let [city-code  (-> m :kunta_nimi city-lookup)
-             population (or (-> m :asukasluku utils/->number) 0)
-             youth      {:investments          (-> m :nuor_investoinnit ->number-div-by-1000)
-                         :net-costs            (-> m :nuor_nettokustannukset ->number-div-by-1000)
-                         :subsidies            (-> m :nuor_avustukset ->number-div-by-1000)
-                         :operating-expenses   (-> m :nuor_kayttokustannukset ->number-div-by-1000)
-                         :operating-incomes    (-> m :nuor_kayttotuotot ->number-div-by-1000)
+   (fn [res m]
+     (let [city-code (-> m :kunta_nimi city-lookup)
+           population (or (-> m :asukasluku utils/->number) 0)
+           youth {:investments (-> m :nuor_investoinnit ->number-div-by-1000)
+                  :net-costs (-> m :nuor_nettokustannukset ->number-div-by-1000)
+                  :subsidies (-> m :nuor_avustukset ->number-div-by-1000)
+                  :operating-expenses (-> m :nuor_kayttokustannukset ->number-div-by-1000)
+                  :operating-incomes (-> m :nuor_kayttotuotot ->number-div-by-1000)
                          ;; New names since 2021 Numbers are not
                          ;; probably comparatible with earlier ones and
                          ;; thus new keys
-                         :operational-expenses (-> m :nuor_toimintakulut ->number-div-by-1000)
-                         :operational-income   (-> m :nuor_toimintatuotot ->number-div-by-1000)
-                         :surplus              (-> m :nuor_tilikauden_ylijaama ->number-div-by-1000)
-                         :deficit              (-> m :nuor_tilikauden_alijaama ->number-div-by-1000)}
-             sport      {:investments          (-> m :liik_investoinnit ->number-div-by-1000)
-                         :net-costs            (-> m :liik_nettokustannukset ->number-div-by-1000)
-                         :subsidies            (-> m :liik_avustukset ->number-div-by-1000)
-                         :operating-expenses   (-> m :liik_kayttokustannukset ->number-div-by-1000)
-                         :operating-incomes    (-> m :liik_kayttotuotot ->number-div-by-1000)
+                  :operational-expenses (-> m :nuor_toimintakulut ->number-div-by-1000)
+                  :operational-income (-> m :nuor_toimintatuotot ->number-div-by-1000)
+                  :surplus (-> m :nuor_tilikauden_ylijaama ->number-div-by-1000)
+                  :deficit (-> m :nuor_tilikauden_alijaama ->number-div-by-1000)}
+           sport {:investments (-> m :liik_investoinnit ->number-div-by-1000)
+                  :net-costs (-> m :liik_nettokustannukset ->number-div-by-1000)
+                  :subsidies (-> m :liik_avustukset ->number-div-by-1000)
+                  :operating-expenses (-> m :liik_kayttokustannukset ->number-div-by-1000)
+                  :operating-incomes (-> m :liik_kayttotuotot ->number-div-by-1000)
                          ;; New names since 2021 Numbers are not
                          ;; probably comparatible with earlier ones and
                          ;; thus new keys
-                         :operational-expenses (-> m :liik_toimintakulut ->number-div-by-1000)
-                         :operational-income   (-> m :liik_toimintatuotot ->number-div-by-1000)
-                         :surplus              (-> m :liik_tilikauden_ylijaama ->number-div-by-1000)
-                         :deficit              (-> m :liik_tilikauden_alijaama ->number-div-by-1000)}]
+                  :operational-expenses (-> m :liik_toimintakulut ->number-div-by-1000)
+                  :operational-income (-> m :liik_toimintatuotot ->number-div-by-1000)
+                  :surplus (-> m :liik_tilikauden_ylijaama ->number-div-by-1000)
+                  :deficit (-> m :liik_tilikauden_alijaama ->number-div-by-1000)}]
 
-         (-> res
-             (assoc-in [city-code year :population] population)
-             (assoc-in [city-code year :services "youth-services"] youth)
-             (assoc-in [city-code year :services "youth-services-pc"]
-                       (calc-per-capita youth population))
-             (assoc-in [city-code year :services "sports-services"] sport)
-             (assoc-in [city-code year :services "sports-services-pc"]
-                       (calc-per-capita sport population)))))
-     {}
-     csv-data))
+       (-> res
+           (assoc-in [city-code year :population] population)
+           (assoc-in [city-code year :services "youth-services"] youth)
+           (assoc-in [city-code year :services "youth-services-pc"]
+                     (calc-per-capita youth population))
+           (assoc-in [city-code year :services "sports-services"] sport)
+           (assoc-in [city-code year :services "sports-services-pc"]
+                     (calc-per-capita sport population)))))
+   {}
+   csv-data))
 
 (def city-finance-csv-headers
-    ["kunta_nimi"
-     "asukasluku"
-     "liik_investoinnit"
-     #_"liik_kayttokustannukset" ; Until 2021
-     #_"liik_kayttotuotot"       ; Until 2021
-     #_"liik_nettokustannukset"  ; Until 2021
-     "liik_avustukset"
-     "liik_toimintakulut"        ; Since 2022
-     "liik_toimintatuotot"       ; Since 2022
-     "liik_tilikauden_ylijaama"  ; Since 2022
-     "liik_tilikauden_alijaama"  ; Since 2022
-     "nuor_investoinnit"
-     #_"nuor_kayttokustannukset" ; Until 2021
-     #_"nuor_kayttotuotot"       ; Until 2021
-     #_"nuor_nettokustannukset"  ; Until 2021
-     "nuor_avustukset"
-     "nuor_toimintakulut"        ; Since 2022
-     "nuor_toimintatuotot"       ; Since 2022
-     "nuor_tilikauden_ylijaama"  ; Since 2022
-     "nuor_tilikauden_alijaama"  ; Since 2022
-     ])
+  ["kunta_nimi"
+   "asukasluku"
+   "liik_investoinnit"
+   #_"liik_kayttokustannukset" ; Until 2021
+   #_"liik_kayttotuotot" ; Until 2021
+   #_"liik_nettokustannukset" ; Until 2021
+   "liik_avustukset"
+   "liik_toimintakulut" ; Since 2022
+   "liik_toimintatuotot" ; Since 2022
+   "liik_tilikauden_ylijaama" ; Since 2022
+   "liik_tilikauden_alijaama" ; Since 2022
+   "nuor_investoinnit"
+   #_"nuor_kayttokustannukset" ; Until 2021
+   #_"nuor_kayttotuotot" ; Until 2021
+   #_"nuor_nettokustannukset" ; Until 2021
+   "nuor_avustukset"
+   "nuor_toimintakulut" ; Since 2022
+   "nuor_toimintatuotot" ; Since 2022
+   "nuor_tilikauden_ylijaama" ; Since 2022
+   "nuor_tilikauden_alijaama" ; Since 2022
+   ])
 
 (defn ->city-finance-sql-update
   [city-code year m]
-    (let [entry (json/encode m)]
-      (format
-       (str
-        "UPDATE city "
-        "SET stats = jsonb_set(stats, '{%s}', '%s') "
-        "WHERE city_code = %s;")
-       year entry city-code)))
+  (let [entry (json/encode m)]
+    (format
+     (str
+      "UPDATE city "
+      "SET stats = jsonb_set(stats, '{%s}', '%s') "
+      "WHERE city_code = %s;")
+     year entry city-code)))
 
 (defn add-city-stats-from-csv!
   [{:keys [db _search] :as system} csv-path year]
   (log/info "Reading city finance entries for year" year "from" csv-path)
-  (let [csv-data  (->> csv-path
-                       slurp
-                       csv/read-csv
-                       utils/csv-data->maps
-                       (map walk/keywordize-keys))
+  (let [csv-data (->> csv-path
+                      slurp
+                      csv/read-csv
+                      utils/csv-data->maps
+                      (map walk/keywordize-keys))
         stats-map (->city-stats-map csv-data year)]
 
     ;; Validate that all columns names were matched
@@ -333,11 +333,10 @@
     (assert (= (into #{} (map keyword) city-finance-csv-headers)
                (into #{} (mapcat keys) csv-data)))
 
-
     (log/info "Storing city finance entries into the db")
     (jdbc/with-db-transaction [tx db]
       (doseq [[city-code m] stats-map
-              [year stats]  m]
+              [year stats] m]
         (let [sql (->city-finance-sql-update city-code year stats)]
           (jdbc/execute! tx [sql]))))
 
@@ -355,10 +354,10 @@
 (defn merge-cities
   [{:keys [db user]} from-city-code to-city-code event-date]
   (let [from-city-code (utils/->int from-city-code)
-        to-city-code   (utils/->int to-city-code)
-        event-date     (or event-date (utils/timestamp))
-        from-name      (get-city-name from-city-code)
-        to-name        (get-city-name to-city-code)]
+        to-city-code (utils/->int to-city-code)
+        event-date (or event-date (utils/timestamp))
+        from-name (get-city-name from-city-code)
+        to-name (get-city-name to-city-code)]
     (log/info "Merging" from-name from-city-code "->" to-name to-city-code)
     (->> from-city-code
          (db/get-sports-sites-by-city-code db)
@@ -371,11 +370,11 @@
 
 (def tasks
   {:index-city-finance-data index-city-finance-data!
-   :index-subsidies         index-subsidies!
-   :merge-cities            merge-cities})
+   :index-subsidies index-subsidies!
+   :merge-cities merge-cities})
 
 (defn print-usage! []
-  (println "\nUsage: lein run -m lipas.maintenance :task-name\n")
+  (println "\nUsage: clojure -M -m lipas.maintenance :task-name\n")
   (println "Available tasks:")
   (doseq [task (keys tasks)]
     (println task)))
@@ -383,10 +382,10 @@
 (defn run-task! [task-fn args]
   (let [config (select-keys config/system-config [:lipas/db :lipas/search])
         system (backend/start-system! config)
-        db     (:lipas/db system)
+        db (:lipas/db system)
         search (:lipas/search system)
-        user   (core/get-user db "import@lipas.fi")
-        args'  {:db db :search search :user user}]
+        user (core/get-user db "import@lipas.fi")
+        args' {:db db :search search :user user}]
     (try
       (apply task-fn (into [args'] args))
       (catch Exception e (log/error e))
@@ -397,7 +396,7 @@
 
 (defn -main [& args]
   (let [task-key (-> args first edn/read-string)
-        args     (rest args)]
+        args (rest args)]
     (if-let [task (get tasks task-key)]
       (run-task! task args)
       (print-usage!))))
@@ -416,10 +415,10 @@
 
   (utils/->prefix-map
    (calc-per-capita
-    {:net-costs          17.0,
-     :subsidies          4.0,
-     :investments        0.0,
-     :operating-incomes  0.0,
+    {:net-costs 17.0,
+     :subsidies 4.0,
+     :investments 0.0,
+     :operating-incomes 0.0,
      :operating-expenses 17.0}
     2500)
    "kissa-komodo-")
@@ -443,26 +442,24 @@
       (let [cities (db/get-cities db)]
 
         (doseq [{:keys [city-code stats]} cities
-                [year stats*]      stats
-                :when             (contains? years year)]
+                [year stats*] stats
+                :when (contains? years year)]
           (log/info "Updating city" city-code "year" year)
-          (let [youth       (get-in stats* [:services :youth-services])
-                sport       (get-in stats* [:services :sports-services])
-                pop         (get stats* :population)
+          (let [youth (get-in stats* [:services :youth-services])
+                sport (get-in stats* [:services :sports-services])
+                pop (get stats* :population)
                 fixed-stats (-> stats*
                                 (assoc-in [:services :youth-services-pc]
                                           (calc-per-capita youth pop))
                                 (assoc-in [:services :sports-services-pc]
                                           (calc-per-capita sport pop)))
-                sql         (->city-finance-sql-update city-code year fixed-stats)]
+                sql (->city-finance-sql-update city-code year fixed-stats)]
             (jdbc/execute! tx [sql])))))
     (log/info "All done!"))
 
   (fix-city-stats-db-entries (user/db) #{2021 2022 2023})
 
-  *e
-
-  )
+  *e)
 
 (comment
   (def csv-path "/Users/tipo/lipas/aineistot/taloustiedot/taloustiedot_2023.csv")
@@ -490,9 +487,6 @@
   (index-city-finance-data! {:db (user/db) :search (user/search)})
 
   (def lol (->> (core/get-cities (user/db) :no-cache)
-        ->city-finance-entries
-        ))
+                ->city-finance-entries))
 
-  (get (utils/index-by :id lol) "179-2023")
-
-  )
+  (get (utils/index-by :id lol) "179-2023"))
