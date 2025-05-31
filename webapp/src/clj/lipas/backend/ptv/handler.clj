ac61c9efe848a9b71a2e8d6e1b95313c27dc5ffb
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2025-05-31 10:35:29 +0300

    Add fancy WFS tests

--- webapp/src/clj/lipas/backend/ptv/handler.clj
+++ webapp/src/clj/lipas/backend/ptv/handler.clj
@@ -3,67 +3,11 @@
             [lipas.backend.ptv.core :as ptv-core]
             [lipas.roles :as roles]
             [lipas.schema.sports-sites :as sports-sites-schema]
+            [lipas.schema.sports-sites.ptv :as ptv-schema]
             [reitit.coercion.malli]
             [reitit.coercion.spec]))
 
-(defn localized-string-schema [string-props]
-  [:map
-   {:closed true}
-   [:fi {:optional true} [:string string-props]]
-   [:se {:optional true} [:string string-props]]
-   [:en {:optional true} [:string string-props]]])
-
-(def integration-enum
-  [:enum "lipas-managed" "manual"])
-
-(def audit-status-enum
-  [:enum "approved" "changes-requested"])
-
-(def audit-field
-  [:map
-   {:closed true}
-   [:status audit-status-enum]
-   [:feedback [:string {:min 0 :max 1000}]]])
-
-(def ptv-audit
-  [:map
-   {:closed true}
-   [:timestamp [:string {:min 24 :max 30}]] ;; ISO-8601 format timestamps
-   [:auditor-id :string]
-   [:summary {:optional true} audit-field]
-   [:description {:optional true} audit-field]])
-
-(def ptv-meta
-  [:map
-   {:closed true}
-   [:org-id :string]
-   [:sync-enabled :boolean]
-   [:delete-existing {:optional true} :boolean]
-
-   ;; These options aren't used now:
-   ;; TODO: Remove
-   [:service-channel-integration
-    {:optional true}
-    integration-enum]
-   [:service-integration
-    {:optional true}
-    integration-enum]
-
-   [:service-channel-ids [:vector :string]]
-   [:service-ids [:vector :string]]
-   ;; [:languages [:vector :string]]
-
-   [:summary (localized-string-schema {:max 150})]
-   [:description (localized-string-schema {})]
-
-   [:audit {:optional true} ptv-audit]])
-
-(def create-ptv-service-location
-  [:map
-   {:closed true}
-   [:org-id :string]
-   [:lipas-id :int]
-   [:ptv ptv-meta]])
+;; Schemas moved to lipas.schema.sports-sites.ptv
 
 (defn routes [{:keys [db search ptv] :as _ctx}]
   [""
@@ -81,7 +25,7 @@
       :handler
       (fn [req]
         {:status 200
-         :body   (ptv-core/get-ptv-integration-candidates search (-> req :parameters :body))})}}]
+         :body (ptv-core/get-ptv-integration-candidates search (-> req :parameters :body))})}}]
 
    ["/actions/generate-ptv-descriptions"
     {:post
@@ -91,9 +35,9 @@
       :handler
       (fn [req]
         {:status 200
-         :body   (ptv-core/generate-ptv-descriptions
-                   search
-                   (-> req :parameters :body :lipas-id))})}}]
+         :body (ptv-core/generate-ptv-descriptions
+                search
+                (-> req :parameters :body :lipas-id))})}}]
 
    ["/actions/generate-ptv-descriptions-from-data"
     {:post
@@ -103,8 +47,8 @@
       :handler
       (fn [req]
         {:status 200
-         :body   (ptv-core/generate-ptv-descriptions-from-data
-                   (-> req :parameters :body))})}}]
+         :body (ptv-core/generate-ptv-descriptions-from-data
+                (-> req :parameters :body))})}}]
 
    ["/actions/translate-to-other-langs"
     {:post
@@ -117,8 +61,8 @@
       :handler
       (fn [req]
         {:status 200
-         :body   (ptv-core/translate-to-other-langs
-                   (-> req :parameters :body))})}}]
+         :body (ptv-core/translate-to-other-langs
+                (-> req :parameters :body))})}}]
 
    ["/actions/generate-ptv-service-descriptions"
     {:post
@@ -130,15 +74,15 @@
                                       :description "Use this to replace the AI input with non-saved site information"}
                            [:maybe
                             [:map
-                             [:city-name (localized-string-schema nil)]
-                             [:service-name (localized-string-schema nil)]
+                             [:city-name (ptv-schema/localized-string-schema nil)]
+                             [:service-name (ptv-schema/localized-string-schema nil)]
                              [:sports-facilties [:vector
                                                  [:map
                                                   [:type :string]]]]]]]]}
       :handler
       (fn [req]
         {:status 200
-         :body   (ptv-core/generate-ptv-service-descriptions search (-> req :parameters :body))})}}]
+         :body (ptv-core/generate-ptv-service-descriptions search (-> req :parameters :body))})}}]
 
    ["/actions/fetch-ptv-org"
     {:post
@@ -148,7 +92,7 @@
       :handler
       (fn [req]
         {:status 200
-         :body   (ptv-core/fetch-ptv-org ptv (-> req :parameters :body :org-id))})}}]
+         :body (ptv-core/fetch-ptv-org ptv (-> req :parameters :body :org-id))})}}]
 
    ["/actions/fetch-ptv-service-collections"
     {:post
@@ -158,7 +102,7 @@
       :handler
       (fn [req]
         {:status 200
-         :body   (ptv-core/fetch-ptv-service-collections ptv (-> req :parameters :body :org-id))})}}]
+         :body (ptv-core/fetch-ptv-service-collections ptv (-> req :parameters :body :org-id))})}}]
 
    ["/actions/save-ptv-service"
     {:post
@@ -169,12 +113,12 @@
                           [:source-id :string]
                           [:sub-category-id :int]
                           [:languages [:vector [:enum "fi" "se" "en"]]]
-                          [:summary (localized-string-schema {:max 150})]
-                          [:description (localized-string-schema nil)]]}
+                          [:summary (ptv-schema/localized-string-schema {:max 150})]
+                          [:description (ptv-schema/localized-string-schema nil)]]}
       :handler
       (fn [req]
         {:status 200
-         :body   (ptv-core/upsert-ptv-service! ptv (-> req :parameters :body))})}}]
+         :body (ptv-core/upsert-ptv-service! ptv (-> req :parameters :body))})}}]
 
    ["/actions/fetch-ptv-services"
     {:post
@@ -184,7 +128,7 @@
       :handler
       (fn [req]
         {:status 200
-         :body   (ptv-core/fetch-ptv-services ptv (-> req :parameters :body :org-id))})}}]
+         :body (ptv-core/fetch-ptv-services ptv (-> req :parameters :body :org-id))})}}]
 
    ["/actions/fetch-ptv-service-channels"
     {:post
@@ -194,7 +138,7 @@
       :handler
       (fn [req]
         {:status 200
-         :body   (ptv-core/fetch-ptv-service-channels ptv (-> req :parameters :body :org-id))})}}]
+         :body (ptv-core/fetch-ptv-service-channels ptv (-> req :parameters :body :org-id))})}}]
 
    ["/actions/fetch-ptv-service-channel"
     {:post
@@ -205,18 +149,18 @@
       :handler
       (fn [req]
         {:status 200
-         :body   (ptv-core/fetch-ptv-service-channel ptv
-                                                     (-> req :parameters :body :org-id)
-                                                     (-> req :parameters :body :service-channel-id))})}}]
+         :body (ptv-core/fetch-ptv-service-channel ptv
+                                                   (-> req :parameters :body :org-id)
+                                                   (-> req :parameters :body :service-channel-id))})}}]
 
    ["/actions/save-ptv-service-location"
     {:post
      {:require-privilege [{:city-code ::roles/any} :ptv/manage]
-      :parameters {:body create-ptv-service-location}
+      :parameters {:body ptv-schema/create-ptv-service-location}
       :handler
       (fn [req]
         {:status 200
-         :body   (ptv-core/upsert-ptv-service-location! db ptv search (:identity req) (-> req :parameters :body))})}}]
+         :body (ptv-core/upsert-ptv-service-location! db ptv search (:identity req) (-> req :parameters :body))})}}]
 
    ["/actions/save-ptv-meta"
     {:post
@@ -226,15 +170,28 @@
       :handler
       (fn [req]
         {:status 200
-         :body   (ptv-core/save-ptv-integration-definitions db search (:identity req) (-> req :parameters :body))})}}]
+         :body (ptv-core/save-ptv-integration-definitions db search (:identity req) (-> req :parameters :body))})}}]
 
    ["/actions/save-ptv-audit"
     {:post
      {:require-privilege :ptv/audit
       :parameters {:body [:map
                           [:lipas-id #'sports-sites-schema/lipas-id]
-                          [:audit #'ptv-audit]]}
+                          [:audit [:map {:closed true}
+                                   [:summary {:optional true} #'ptv-schema/audit-field]
+                                   [:description {:optional true} #'ptv-schema/audit-field]]]]}
       :handler
       (fn [req]
-        {:status 200
-         :body   (ptv-core/save-ptv-audit db search (:identity req) (-> req :parameters :body))})}}]])
+        (let [body (-> req :parameters :body)
+              audit (:audit body)]
+          ;; Additional validation to catch extra fields in audit sub-maps
+          (if (or (and (contains? audit :summary)
+                       (let [summary (:summary audit)]
+                         (some #(not (contains? #{:status :feedback} %)) (keys summary))))
+                  (and (contains? audit :description)
+                       (let [description (:description audit)]
+                         (some #(not (contains? #{:status :feedback} %)) (keys description)))))
+            {:status 400 :body {:error "Invalid audit field: extra keys not allowed"}}
+            (if-let [result (ptv-core/save-ptv-audit db search (:identity req) body)]
+              {:status 200 :body result}
+              {:status 404 :body {:error "Sports site not found"}}))))}}]])
