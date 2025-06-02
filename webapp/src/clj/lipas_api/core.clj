e841e4fa81759c3ac74424cd41875683ff1ef804
Tuomas-Matti Soikkeli <tuomas-matti.soikkeli@siili.com>
2025-06-02 22:32:06 +0300

    Fetch deleted sports sites from ES

--- webapp/src/clj/lipas_api/core.clj
+++ webapp/src/clj/lipas_api/core.clj
@@ -3,7 +3,9 @@
    [lipas-api.search.cli :as es]
    [lipas-api.sports-places :refer [filter-and-format
                                     format-sports-place-es]]
-   [lipas-api.util :refer [only-non-nil-recur] :as util]))
+   [lipas-api.util :refer [only-non-nil-recur] :as util]
+   [qbits.spandex :as elastic]
+   [qbits.spandex.utils :as es-utils]))
 
 (defn fetch-sports-places-es
   "Fetches list of sports-places from ElasticSearch backend."
@@ -31,3 +33,23 @@
         (format-sports-place-es locale)
         only-non-nil-recur)
     (catch Exception ex (println ex))))
+
+(defn fetch-deleted-sports-places-es
+  "Fetches list of deleted sports-places from ElasticSearch backend.
+   Returns sports places that were previously published but are now in non-published status
+   after the given timestamp."
+  [search since-timestamp]
+  (let [client (:client search)
+        response (elastic/request client
+                                  {:method :get
+                                   :url (es-utils/url [:legacy_sports_sites_current :_search])
+                                   :body {:query {:bool {:must [{:range {:lastModified {:gte since-timestamp}}}]
+                                                         :must_not [{:term {:status "published"}}]}}
+                                          :sort [{:lastModified {:order "desc"}}]
+                                          :_source ["sportsPlaceId" "lastModified"]}})
+        hits (-> response :body :hits :hits)]
+    (map (fn [hit]
+           (let [source (:_source hit)]
+             {:sportsPlaceId (:sportsPlaceId source)
+              :deletedAt (str (:lastModified source))}))
+         hits)))
