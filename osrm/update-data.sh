c5c9204c0209ad5660f40493918fd25773ae785e
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2025-07-04 18:58:12 +0300

    Update to latest osrm-backend

--- osrm/update-data.sh
+++ osrm/update-data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 source ../.env.sh
 
 # Download latest dataset
@@ -7,18 +9,18 @@ wget http://download.geofabrik.de/europe/finland-latest.osm.pbf
 
 # Car
 
-docker run --rm -t -v "${PWD}/car:/data:z" -v "${PWD}/finland-latest.osm.pbf:/data/finland-latest.osm.pbf:z" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/finland-latest.osm.pbf
-docker run --rm -t -v "${PWD}/car:/data:z" osrm/osrm-backend osrm-partition /data/finland-latest.osrm
-docker run --rm -t -v "${PWD}/car:/data:z" osrm/osrm-backend osrm-customize /data/finland-latest.osrm
+docker run --rm -t -v "${PWD}/car:/data:z" -v "${PWD}/finland-latest.osm.pbf:/data/finland-latest.osm.pbf:z" ghcr.io/project-osrm/osrm-backend osrm-extract -p /opt/car.lua /data/finland-latest.osm.pbf
+docker run --rm -t -v "${PWD}/car:/data:z" ghcr.io/project-osrm/osrm-backend osrm-partition /data/finland-latest.osrm
+docker run --rm -t -v "${PWD}/car:/data:z" ghcr.io/project-osrm/osrm-backend osrm-customize /data/finland-latest.osrm
 
 # Foot
 
-docker run --rm -t -v "${PWD}/foot:/data:z" -v "${PWD}/finland-latest.osm.pbf:/data/finland-latest.osm.pbf:z" osrm/osrm-backend osrm-extract -p /opt/foot.lua /data/finland-latest.osm.pbf
-docker run --rm -t -v "${PWD}/foot:/data:z" osrm/osrm-backend osrm-partition /data/finland-latest.osrm
-docker run --rm -t -v "${PWD}/foot:/data:z" osrm/osrm-backend osrm-customize /data/finland-latest.osrm
+docker run --rm -t -v "${PWD}/foot:/data:z" -v "${PWD}/finland-latest.osm.pbf:/data/finland-latest.osm.pbf:z" ghcr.io/project-osrm/osrm-backend osrm-extract -p /opt/foot.lua /data/finland-latest.osm.pbf
+docker run --rm -t -v "${PWD}/foot:/data:z" ghcr.io/project-osrm/osrm-backend osrm-partition /data/finland-latest.osrm
+docker run --rm -t -v "${PWD}/foot:/data:z" ghcr.io/project-osrm/osrm-backend osrm-customize /data/finland-latest.osrm
 
 # Bicycle
 
-docker run --rm -t -v "${PWD}/bicycle:/data:z" -v "${PWD}/finland-latest.osm.pbf:/data/finland-latest.osm.pbf:z" osrm/osrm-backend osrm-extract -p /opt/bicycle.lua /data/finland-latest.osm.pbf
-docker run --rm -t -v "${PWD}/bicycle:/data:z" osrm/osrm-backend osrm-partition /data/finland-latest.osrm
-docker run --rm -t -v "${PWD}/bicycle:/data:z" osrm/osrm-backend osrm-customize /data/finland-latest.osrm
+docker run --rm -t -v "${PWD}/bicycle:/data:z" -v "${PWD}/finland-latest.osm.pbf:/data/finland-latest.osm.pbf:z" ghcr.io/project-osrm/osrm-backend osrm-extract -p /opt/bicycle.lua /data/finland-latest.osm.pbf
+docker run --rm -t -v "${PWD}/bicycle:/data:z" ghcr.io/project-osrm/osrm-backend osrm-partition /data/finland-latest.osrm
+docker run --rm -t -v "${PWD}/bicycle:/data:z" ghcr.io/project-osrm/osrm-backend osrm-customize /data/finland-latest.osrm
